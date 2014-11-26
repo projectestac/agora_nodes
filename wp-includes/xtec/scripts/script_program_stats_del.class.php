464c1c46199d5b032787c8ba900b67367dd41871
Toni Ginard <toni.ginard@gmail.com>
2014-11-26 16:38:40 +0100

    Added support to remove old stats via wp-cron

---
+++ wp-includes/xtec/scripts/script_program_stats_del.class.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once('agora_script_base.class.php');
+
+class script_program_stats_del extends agora_script_base {
+
+    public $title = 'Program stats deletion';
+    public $info = 'Programs the deletion of the old stats in WP cron';
+
+    public function params() {
+        $params = array();
+        $params['name'] = '';
+        return $params;
+    }
+
+    protected function _execute($params = array()) {
+
+        add_action('remove_stats', 'remove_old_stats');
+
+        if (!wp_next_scheduled('remove_stats')) {
+            wp_schedule_event(time(), 'daily', 'remove_stats');
+        }
+        
+        return true;
+    }
+
+}
