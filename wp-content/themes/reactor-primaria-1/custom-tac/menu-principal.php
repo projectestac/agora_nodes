0b8e7b27fba6172c483e5aabb57ebbd46b3f7e6d
Xavier Meler <jmeler@xtec.cat>
2014-12-02 14:48:02 +0100

    Improved main menu items. Fix #58

--- wp-content/themes/reactor-primaria-1/custom-tac/menu-principal.php
+++ wp-content/themes/reactor-primaria-1/custom-tac/menu-principal.php
@@ -48,7 +48,9 @@ function start_lvl( &$output, $depth = 0, $args = array() ) {
     $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
     $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
     $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
-    $attributes .= ' class="menu-link ' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '"';
+    $attributes .= ' class="menu-link ';
+    $attributes .= ($item->url=='#' ? 'no-link ' : '');
+    $attributes .= ($depth > 0 ? 'sub-menu-link' : 'main-menu-link') . '"';
   
     $item_output = sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
         $args->before,
