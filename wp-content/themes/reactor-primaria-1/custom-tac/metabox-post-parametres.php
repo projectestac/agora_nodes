57b71a424adef4010ae86a6aeed715975575f239
Xavier Meler <jmeler@xtec.cat>
2014-12-04 20:09:46 +0100

    Added tumblog icons style to post. Close #61

--- wp-content/themes/reactor-primaria-1/custom-tac/metabox-post-parametres.php
+++ wp-content/themes/reactor-primaria-1/custom-tac/metabox-post-parametres.php
@@ -35,16 +35,32 @@ function metabox1_callback( $post ) {
 	 * Use get_post_meta() to retrieve an existing value
 	 * from the database and use the value for the form.
 	 */
-	$check1 = get_post_meta( $post->ID, '_amaga_titol', true );
-	$check2 = get_post_meta( $post->ID, '_amaga_metadata', true );
-	$check3 = get_post_meta( $post->ID, '_bloc_html', true );
-        $check4 = get_post_meta( $post->ID, '_original_size', true );
+	$ck_hide_title = get_post_meta( $post->ID, '_amaga_titol', true );
+	$ck_hide_metadata = get_post_meta( $post->ID, '_amaga_metadata', true );
+	$ck_bloc_html = get_post_meta( $post->ID, '_bloc_html', true );
+        $ck_original_size = get_post_meta( $post->ID, '_original_size', true );
         
-        echo '<input type="checkbox" id="original_size" name="original_size" '.checked( $check4,'on' ,false ).'/> Imatge destacada amb mida original<br>';
-	echo '<input type="checkbox" id="bloc_html" name="bloc_html" '.checked( $check3,'on' ,false ).'/> Mostra contingut sencer, no un resum<br>';
-	echo '<input type="checkbox" id="amaga_titol" name="amaga_titol" '.checked( $check1,'on', false ).' /> Amaga títol<br>';
-	echo '<input type="checkbox" id="amaga_metadata" name="amaga_metadata" '.checked( $check2,'on',false ).'/> Amaga metadades';
-	
+        echo '<input type="checkbox" id="original_size" name="original_size" '.checked( $ck_original_size,'on' ,false ).'/> Imatge destacada amb mida original<br>';
+	echo '<input type="checkbox" id="bloc_html" name="bloc_html" '.checked( $ck_bloc_html,'on' ,false ).'/> Mostra contingut sencer, no un resum<br>';
+	echo '<input type="checkbox" id="amaga_titol" name="amaga_titol" '.checked( $ck_hide_title,'on', false ).' /> Amaga títol<br>';
+	echo '<input type="checkbox" id="amaga_metadata" name="amaga_metadata" '.checked( $ck_hide_metadata,'on',false ).'/> Amaga metadades';
+
+        $entry_icon=get_post_meta( $post->ID, '_entry_icon', true );
+        ?>
+        <br>Icona:
+        <select id="entry_icon" name="entry_icon">
+            <option value="noicon" <?php selected( $entry_icon, "noicon" ); ?>></option>
+            <option value="video" <?php selected( $entry_icon, "video" ); ?>>Video</option>
+            <option value="carrusel" <?php selected( $entry_icon, "carrusel" ); ?>>Carrusel</option>
+            <option value="podcast" <?php selected( $entry_icon, "podcast" ); ?>>Podcast</option>
+            <option value="document" <?php selected( $entry_icon, "document" ); ?>>Document</option>
+            <option value="musica" <?php selected( $entry_icon, "musica" ); ?>>Música</option>
+            <option value="alerta" <?php selected( $entry_icon, "alerta" ); ?>>Alerta</option>
+        </select>
+
+
+        <?php
+        
 }
 
 /**
@@ -100,12 +116,14 @@ function metabox1_savedata( $post_id ) {
 	$mostrar_metadata= $_POST['amaga_metadata'] ;
 	$bloc_html= $_POST['bloc_html'] ;
         $original_size= $_POST['original_size'] ;
+        $entry_icon=$_POST['entry_icon'];
 
 	// Update the meta field in the database.
 	update_post_meta( $post_id, '_amaga_titol', $mostrar_titol);
 	update_post_meta( $post_id, '_amaga_metadata', $mostrar_metadata);
 	update_post_meta( $post_id, '_bloc_html', $bloc_html);
         update_post_meta( $post_id, '_original_size', $original_size);
+        update_post_meta( $post_id, '_entry_icon', $entry_icon);
 	
 }
 add_action( 'save_post', 'metabox1_savedata' );
