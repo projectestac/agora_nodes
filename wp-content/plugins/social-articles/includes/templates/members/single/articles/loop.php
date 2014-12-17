5ee4030dc0821412374374eb8add2144c4aafe54
Toni Ginard <toni.ginard@gmail.com>
2014-12-17 17:12:13 +0100

    Upgraded Social Articles to version 1.7

--- wp-content/plugins/social-articles/includes/templates/members/single/articles/loop.php
+++ wp-content/plugins/social-articles/includes/templates/members/single/articles/loop.php
@@ -23,33 +23,13 @@
            $postCount =  count_user_posts(bp_displayed_user_id());
        }
 
-
-      define(PUBLISH, "publish");
-      define(PENDING, "pending");
-      define(DRAFT, "draft");
 ?>
 
 <section id="articles-container">     
     <?php if($publishCount > 0 || bp_displayed_user_id()==bp_loggedin_user_id()):?>    
-    <div class="item-list-tabs no-ajax">                          
-        <ul class="nav nav-tabs">                    
-            <li id="publish-tab" class="current">
-                <a href="#" onclick="showContent('publish'); return false;"><?php _e("Published", "social-articles");?> <span id="publish-count"><?php echo $publishCount;?></span></a>
-            </li>
-            <?php if(bp_displayed_user_id()==bp_loggedin_user_id()):?>
-            <?php if(!$directWorkflow): ?>
-            <li id="pending-tab">
-                <a href="#" onclick="showContent('pending'); return false;"><?php _e("Under Review", "social-articles");?> <span id="pending-count"><?php echo $pendingCount;?></span></a>
-            </li>
-            <?php endif;?>
-            <li id="draft-tab">
-                <a href="#" onclick="showContent('draft'); return false;"><?php _e("Draft", "social-articles");?> <span id="draft-count"><?php echo $draftCount;?></span></a>
-            </li>           
-            <?php endif;?>                   
-        </ul>                                
-    </div>     
+
     <div class="publish-container">    
-        <?php get_articles(0, PUBLISH);?>    
+        <?php get_articles(0, 'publish');?>
         <div id="more-container-publish">
         </div>    
         <?php
@@ -62,21 +42,11 @@
         }
         ?>
     </div>
-    <?php if(bp_displayed_user_id()==bp_loggedin_user_id()):?>
-    <?php if(!$directWorkflow): ?>
-     <div class="pending-container">
-        <?php get_articles(0, PENDING, true);?>             
-     </div>
-    <?php endif?>
-     <div class="draft-container">    
-        <?php get_articles(0, DRAFT, true);?>          
-     </div>
-    <?php endif;?> 
     <?php else: ?>
     <div id="message" class="messageBox note icon">
         <span><?php _e("This user doesn't have any article.", "social-articles");?></span>
     </div>
-    <?php endif;?>    
+    <?php endif;?>
 </section>     
 
 <input type="hidden" value="<?php echo $initialCount;?>" id="inicialcount"/>
