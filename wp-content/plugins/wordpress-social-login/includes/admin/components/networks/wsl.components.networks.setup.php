8af8352f2b08b2b7386c3dd9af8d7920515db0a7
Sara Arjona <sarjona@xtec.cat>
2015-02-13 14:22:00 +0100

    NODES: Reviewed Social Login help to add different URL for XTECBlocs and Nodes

--- wp-content/plugins/wordpress-social-login/includes/admin/components/networks/wsl.components.networks.setup.php
+++ wp-content/plugins/wordpress-social-login/includes/admin/components/networks/wsl.components.networks.setup.php
@@ -347,18 +347,20 @@ function wsl_component_networks_setup()
                 <!--// XTEC ************ AFEGIT - Customize help info for Moodle and Google providers. Not using gettext, sorry! :( -->
                 <!--// 2014.11.19 @aginard -->
                 <?php else : if ($provider_id == 'Moodle') : ?>
+                <?php $help_moodle= (empty($isAgora) || $isAgora)?'http://agora.xtec.cat/moodle/moodle/mod/glossary/view.php?id=1741&mode=entry&hook=2228':'https://sites.google.com/a/xtec.cat/ajudaxtecblocs/usuaris/configuracio-d-acces-a-xtecblocs-amb-els-usuaris-del-moodle'; ?> 
                 <div
                     class="wsl_div_settings_help_<?php echo $provider_id; ?>"
 					style="<?php if( isset( $_REQUEST["enable"] ) && ! isset( $_REQUEST["settings-updated"] ) && $_REQUEST["enable"] == $provider_id ) echo "-"; // <= lolz ?>display:none;">
                     <hr class="wsl" />
-                    <p>Per fer que els usuaris del Moodle puguin entrar a l'espai Nodes automàticament, visiteu la pàgina <a href="http://agora.xtec.cat/moodle/moodle/mod/glossary/view.php?id=1302&mode=cat&hook=201" target="_blank">http://agora.xtec.cat/moodle/moodle/mod/glossary/view.php?id=1302&mode=cat&hook=201</a> i seguiu les instruccions.</p><p>Heu de tenir en compte que la primera vegada que un usuari/ària del Moodle entri, se li crearà automàticament un compte a Nodes en cas que no en tingui.</p>
+                    <p>Per fer que els usuaris del Moodle puguin entrar a aquest espai automàticament, visiteu <a href="<?php echo $help_moodle?>" target="_blank">aquesta pàgina</a> i seguiu les instruccions.</p><p>La primera vegada que un usuari/ària del Moodle entri, se li crearà automàticament un compte a aquest espai en cas que no en tingui.</p>
                 </div>
                 <?php endif; if ($provider_id == 'Google') : ?>
+                <?php $help_google= (empty($isAgora) || $isAgora)?'http://agora.xtec.cat/moodle/moodle/mod/glossary/view.php?id=1741&mode=entry&hook=2281':'https://sites.google.com/a/xtec.cat/ajudaxtecblocs/usuaris/configuracio-d-acces-a-xtecblocs-amb-usuaris-de-google'; ?> 
                 <div
                     class="wsl_div_settings_help_<?php echo $provider_id; ?>"
 					style="<?php if( isset( $_REQUEST["enable"] ) && ! isset( $_REQUEST["settings-updated"] ) && $_REQUEST["enable"] == $provider_id ) echo "-"; // <= lolz ?>display:none;">
                     <hr class="wsl" />
-                    <p>Per fer que els usuaris del Google puguin entrar a l'espai Nodes automàticament, visiteu la pàgina <a href="http://agora.xtec.cat/moodle/moodle/mod/glossary/view.php?id=1741&mode=entry&hook=2281" target="_blank">http://agora.xtec.cat/moodle/moodle/mod/glossary/view.php?id=1741&mode=entry&hook=2281</a> i seguiu les instruccions.</p><p>Heu de tenir en compte que la primera vegada que un usuari/ària del Google entri, se li crearà automàticament un compte a Nodes en cas que no en tingui.</p>
+                    <p>Per fer que els usuaris del Google puguin entrar a aquest espai automàticament, visiteu <a href="<?php echo $help_google?>" target="_blank">aquesta pàgina</a> i seguiu les instruccions.</p><p>La primera vegada que un usuari/ària del Google entri, se li crearà automàticament un compte a aquest espai en cas que no en tingui.</p>
                 </div>
                 <?php endif; endif; ?>
                 <!--//************ FI -->
