87ca46b8853c3f58c4dc52d22a58282cfe2c9924
Sara Arjona <sarjona@xtec.cat>
2015-02-13 11:22:09 +0100

    NODES: Reviewed menus to simplify code

--- wp-admin/menu.php
+++ wp-admin/menu.php
@@ -188,9 +188,7 @@ if ( ! is_multisite() && current_user_can( 'update_plugins' ) ) {
 
 // XTEC ************ AFEGIT - Block access to plugin management to all users but xtecadmin
 // 2014.10.21 @aginard
-global $isAgora;
-
-if (($isAgora && is_xtecadmin()) || !$isAgora) {
+if (!$isAgora || is_xtecadmin()) {
 //************ FI
 
 $menu[65] = array( sprintf( __('Plugins %s'), $count ), 'activate_plugins', 'plugins.php', '', 'menu-top menu-icon-plugins', 'menu-plugins', 'dashicons-admin-plugins' );
@@ -250,14 +248,11 @@ $menu[80] = array( __('Settings'), 'manage_options', 'options-general.php', '', 
 	$submenu['options-general.php'][20] = array(__('Reading'), 'manage_options', 'options-reading.php');
 	$submenu['options-general.php'][25] = array(__('Discussion'), 'manage_options', 'options-discussion.php');
 	$submenu['options-general.php'][30] = array(__('Media'), 'manage_options', 'options-media.php');
-
 // XTEC ************ AFEGIT - Block access to permalink management to all users but xtecadmin
 // 2014.11.03 @sarjona
-if (($isAgora && is_xtecadmin()) || !$isAgora) {
+if (!$isAgora || is_xtecadmin()) {
 //************ FI
-    
 	$submenu['options-general.php'][40] = array(__('Permalinks'), 'manage_options', 'options-permalink.php');
-
 // XTEC ************ AFEGIT - Block access to permalink management to all users but xtecadmin
 // 2014.11.03 @sarjona
 }
