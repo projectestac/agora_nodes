713026e235626c278bb21b134a494a598c7cd3e2
Xavier Meler <jmeler@xtec.cat>
2015-02-16 11:51:25 +0100

    Clear unused css rules

--- wp-content/themes/reactor-primaria-1/sidebar-frontpage-2.php
+++ wp-content/themes/reactor-primaria-1/sidebar-frontpage-2.php
@@ -8,12 +8,10 @@
  */
 ?>
 
-    <?php reactor_sidebar_before(); ?>
-    
-        <div id="sidebar-frontpage-2" class="sidebar <?php reactor_columns( 2 ); ?>" role="complementary">
-    		<?php dynamic_sidebar('sidebar-frontpage-2'); ?>
-        </div><!-- #sidebar-frontpage -->
-        
-    <?php reactor_sidebar_after(); ?>
+<?php reactor_sidebar_before(); ?>
+    <div id="sidebar-frontpage-2" class="sidebar <?php reactor_columns( 2 ); ?>" role="complementary">
+        <?php dynamic_sidebar('sidebar-frontpage-2'); ?>
+    </div><!-- #sidebar-frontpage -->
+<?php reactor_sidebar_after(); ?>
     
     
