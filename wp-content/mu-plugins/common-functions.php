425fcabdf982aba810eac02299b73ec9cada4e91
Toni Ginard <toni.ginard@gmail.com>
2015-04-02 15:54:11 +0200

    Removed unused var

--- wp-content/mu-plugins/common-functions.php
+++ wp-content/mu-plugins/common-functions.php
@@ -148,9 +148,18 @@ add_action('pre_get_posts','users_own_attachments');
  * Disable gravatar.com calls.
  * @author Víctor Saavedra (vsaavedr@xtec.cat)
  */
-function remove_gravatar ($avatar, $id_or_email, $size, $default, $alt) {
-	$default = admin_url('images/mysteryman.png');
-	return "<img alt='{$alt}' src='{$default}' class='avatar avatar-{$size} photo avatar-default' height='{$size}' width='{$size}' />";
+function remove_contributor_dashboard () {
+	$user_id = get_current_user_id();
+
+	$caps = get_user_meta($user_id, 'wp_capabilities', true);
+	$roles = array_keys((array)$caps);
+	$role = $roles[0];
+
+	if ($role === 'contributor') {
+		remove_menu_page('edit-comments.php');
+		remove_menu_page('edit.php?post_type=gce_feed');
+		remove_menu_page('tools.php');
+	}
 }
 
 add_filter('get_avatar', 'remove_gravatar', 1, 5);
