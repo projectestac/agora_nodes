164fb0bf90a90b7f1c97aa4eface27480292fa02
Toni Ginard <toni.ginard@gmail.com>
2015-04-02 13:24:57 +0200

    Fixed merge conflicts and added other changes from master

--- wp-content/mu-plugins/common-functions.php
+++ wp-content/mu-plugins/common-functions.php
@@ -145,22 +145,12 @@ function users_own_attachments( $wp_query_obj ) {
 add_action('pre_get_posts','users_own_attachments');
 
 /**
- * Remove options from the admin menu for user contributor
- * @author Nacho Abejaro
+ * Disable gravatar.com calls.
+ * @author Víctor Saavedra (vsaavedr@xtec.cat)
  */
-function remove_contributor_dashboard () {
-	global $current_user;
-
-	$user_id = get_current_user_id();
-
-	$caps = get_user_meta($user_id, 'wp_capabilities', true);
-	$roles = array_keys((array)$caps);
-	$role = $roles[0];
-
-	if ($role === 'contributor') {
-		remove_menu_page('edit-comments.php');
-		remove_menu_page('edit.php?post_type=gce_feed');
-		remove_menu_page('tools.php');
-	}
+function remove_gravatar ($avatar, $id_or_email, $size, $default, $alt) {
+	$default = admin_url('images/mysteryman.png');
+	return "<img alt='{$alt}' src='{$default}' class='avatar avatar-{$size} photo avatar-default' height='{$size}' width='{$size}' />";
 }
-add_action('admin_menu', 'remove_contributor_dashboard');
+
+add_filter('get_avatar', 'remove_gravatar', 1, 5);
