1d889b83a4879d01c92d087f390f596ea1175930
pau.ferrer-ocana <crazyserver@gmail.com>
2015-04-14 17:01:37 +0200

    Script to enable service

--- site-config.php
+++ site-config.php
@@ -10,6 +10,7 @@ global $agora, $isAgora, $isBlocs;
 $isAgora = true;
 $isBlocs = false;
 
+define('CENTRE', $centre);
 define('DB_NAME', $agora['nodes']['userprefix'] . $school_info['id_nodes']);
 define('DB_HOST', $school_info['dbhost_nodes']);
 define('WP_SITEURL', $agora['server']['html'] . $centre . '/');
