e0725775365e55ca947326e161229cd2c9929943
Toni Ginard <toni.ginard@gmail.com>
2015-05-07 13:00:01 +0200

    Recovered lost changes in agora-functions

--- wp-content/mu-plugins/agora-functions.php
+++ wp-content/mu-plugins/agora-functions.php
@@ -1,11 +1,12 @@
 <?php
+
 /*
-Plugin Name: AgoraFunctions
-Plugin URI: https://github.com/projectestac/agora_nodes
-Description: A pluggin to include specific functions which affects only to Àgora-Nodes
-Version: 1.0
-Author: Àrea TAC - Departament d'Ensenyament de Catalunya
-*/
+  Plugin Name: AgoraFunctions
+  Plugin URI: https://github.com/projectestac/agora_nodes
+  Description: A pluggin to include specific functions which affects only to Àgora-Nodes
+  Version: 1.0
+  Author: Àrea TAC - Departament d'Ensenyament de Catalunya
+ */
 
 load_muplugin_textdomain('agora-functions', '/languages');
 
@@ -16,8 +17,9 @@ load_muplugin_textdomain('agora-functions', '/languages');
  * @author Sara Arjona
  */
 function bp_docs_get_create_link_filter($url) {
-	return preg_replace('/^http:/i', 'https:', $url);
+    return preg_replace('/^http:/i', 'https:', $url);
 }
+
 add_filter('bp_docs_get_create_link', 'bp_docs_get_create_link_filter');
 
 /**
@@ -28,21 +30,22 @@ function bp_profile_menu_posts() {
     global $bp;
 
     bp_core_new_nav_item(
-    	array(
-            'name' => __('My Articles', 'agora-functions'),
-            'slug' => 'myposts',
-            'position' => 100,
-            'default_subnav_slug' => 'published',
-            'screen_function' => 'mb_author_posts'
-    	)
+            array(
+                'name' => __('My Articles', 'agora-functions'),
+                'slug' => 'myposts',
+                'position' => 100,
+                'default_subnav_slug' => 'published',
+                'screen_function' => 'mb_author_posts'
+            )
     );
 }
-add_action('bp_setup_nav', 'bp_profile_menu_posts', 301 );
+
+add_action('bp_setup_nav', 'bp_profile_menu_posts', 301);
 
 /**
  * Build two sub menu items, the first is showing by default
  * @author Nacho Abejaro
-*/
+ */
 function bp_profile_submenu_posts() {
     global $bp;
 
@@ -51,39 +54,40 @@ function bp_profile_submenu_posts() {
     $draftCount = get_user_posts_count('draft');
 
     bp_core_new_subnav_item(
-        array(
-            'name' => __('Published', 'agora-functions').'<span>'.$publishCount.'</span>',
-            'slug' => 'mypublished',
-            'parent_url' => bp_displayed_user_domain() . $bp->bp_nav['myposts']['slug'] . '/',
-            'parent_slug' => $bp->bp_nav['myposts']['slug'],
-            'position' => 10,
-            'screen_function' => 'mb_author_posts' // the function is declared below
-        )
+            array(
+                'name' => __('Published', 'agora-functions') . '<span>' . $publishCount . '</span>',
+                'slug' => 'mypublished',
+                'parent_url' => bp_displayed_user_domain() . $bp->bp_nav['myposts']['slug'] . '/',
+                'parent_slug' => $bp->bp_nav['myposts']['slug'],
+                'position' => 10,
+                'screen_function' => 'mb_author_posts' // the function is declared below
+            )
     );
 
     bp_core_new_subnav_item(
-	    array(
-		    'name' => __('Drafts', 'agora-functions').'<span>'.$pendingCount.'</span>',
-		    'slug' => 'myunder-review',
-		    'parent_url' => bp_displayed_user_domain() . $bp->bp_nav['myposts']['slug'] . '/',
-		    'parent_slug' => $bp->bp_nav['myposts']['slug'],
-		    'position' => 20,
-		    'screen_function' => 'mb_author_pending' // the function is declared below
-	    )
+            array(
+                'name' => __('Pending Review', 'agora-functions') . '<span>' . $pendingCount . '</span>',
+                'slug' => 'myunder-review',
+                'parent_url' => bp_displayed_user_domain() . $bp->bp_nav['myposts']['slug'] . '/',
+                'parent_slug' => $bp->bp_nav['myposts']['slug'],
+                'position' => 20,
+                'screen_function' => 'mb_author_pending' // the function is declared below
+            )
     );
 
     bp_core_new_subnav_item(
-        array(
-            'name' => __('Drafts', 'agora-functions').'<span>'.$draftCount.'</span>',
-            'slug' => 'mydrafts',
-            'parent_url' => bp_displayed_user_domain() . $bp->bp_nav['myposts']['slug'] . '/',
-            'parent_slug' => $bp->bp_nav['myposts']['slug'],
-            'position' => 30,
-            'screen_function' => 'mb_author_drafts' // the function is declared below
-        )
+            array(
+                'name' => __('Drafts', 'agora-functions') . '<span>' . $draftCount . '</span>',
+                'slug' => 'mydrafts',
+                'parent_url' => bp_displayed_user_domain() . $bp->bp_nav['myposts']['slug'] . '/',
+                'parent_slug' => $bp->bp_nav['myposts']['slug'],
+                'position' => 30,
+                'screen_function' => 'mb_author_drafts' // the function is declared below
+            )
     );
 }
-add_action('bp_setup_nav', 'bp_profile_submenu_posts', 302 );
+
+add_action('bp_setup_nav', 'bp_profile_submenu_posts', 302);
 
 /**
  * Manage the first sub item
@@ -142,83 +146,82 @@ function mb_show_pending() {
  */
 function myTemplate($query) {
 
-	// Launch the query
-	query_posts($query);
+    // Launch the query
+    query_posts($query);
 
-	if ( have_posts() ) {
-		while ( have_posts() ) {
-			the_post();
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
 
-			$allCategories = array();
-			$categories = get_the_category();
-			for($i=0; $i < count($categories); $i++){
-				$allCategories[]='<a href="'.get_category_link( $categories[$i]->cat_ID ).'" >'.$categories[$i]->cat_name.'</a>';
-			}
+            $allCategories = array();
+            $categories = get_the_category();
+            for ($i = 0; $i < count($categories); $i++) {
+                $allCategories[] = '<a href="' . get_category_link($categories[$i]->cat_ID) . '" >' . $categories[$i]->cat_name . '</a>';
+            }
 
-			echo '<div style="width: 100%; float: left; position: relative; margin-bottom: 5px;">';
-				echo '<div style="width: 100%; min-height: 90px; float: left; border-bottom: 2px solid #F2F0F0; background: white;	margin-left: 0px; position: relative;">';
-					 echo '<div style="max-width: 100px; max-height: 100px; float: left; margin-top: 10px; position: relative; width: 200%;" >';
-					 echo '<a href="' . get_edit_post_link() . '">';
-                     the_post_thumbnail(thumbnail);
-                     echo '</a>';
-                     echo '</div>';
+            echo '<div style="width: 100%; float: left; position: relative; margin-bottom: 5px;">';
+            echo '<div style="width: 100%; min-height: 90px; float: left; border-bottom: 2px solid #F2F0F0; background: white;	margin-left: 0px; position: relative;">';
+            echo '<div style="max-width: 100px; max-height: 100px; float: left; margin-top: 10px; position: relative; width: 200%;" >';
+            echo '<a href="' . get_edit_post_link() . '">';
+            the_post_thumbnail(thumbnail);
+            echo '</a>';
+            echo '</div>';
 
-                     echo '<div style="float: left; width: 78%; min-height: 105px; margin-top: 5px; margin-left: 2%; position: relative; margin-bottom: 5px;">';
-	                    echo '<h3 style="font-size: 20px !important; margin-top: 5px !important; margin-bottom: 0.1em !important;">';
-	                    echo '<a href="' . get_edit_post_link() . '">';
-	                    the_title();
-	                    echo '</a>';
-	                    echo '</h3>';
+            echo '<div style="float: left; width: 78%; min-height: 105px; margin-top: 5px; margin-left: 2%; position: relative; margin-bottom: 5px;">';
+            echo '<h3 style="font-size: 20px !important; margin-top: 5px !important; margin-bottom: 0.1em !important;">';
+            echo '<a href="' . get_edit_post_link() . '">';
+            the_title();
+            echo '</a>';
+            echo '</h3>';
 
-	                    echo '<span style="float: left; width: 100%; height: 10px; margin-bottom: 5px; color: #A0A5A9; font-size: 11px; font-style: italic; font: 08px "Droid Serif", Georgia, "Times New Roman", Times, serif; text-align: right; ">' . get_the_date() . '</span>';
+            echo '<span style="float: left; width: 100%; height: 10px; margin-bottom: 5px; color: #A0A5A9; font-size: 11px; font-style: italic; font: 08px "Droid Serif", Georgia, "Times New Roman", Times, serif; text-align: right; ">' . get_the_date() . '</span>';
 
-	                    echo '<div class="excerpt">';
-	                    	echo limit_text(get_the_excerpt(), 30);
-	                    echo '</div>';
-                     echo '</div>';
+            echo '<div class="excerpt">';
+            echo limit_text(get_the_excerpt(), 30);
+            echo '</div>';
+            echo '</div>';
 
-                     echo '<div id="article-footer">';
-                     	echo '<div style="width: 50%; height: 30px; float: left; margin-bottom: 2px; font-size:11px; line-height: 30px;">';
-                     		echo __('Categories ', 'agora-functions').implode(" | ",$allCategories);
-                     	echo '</div>';
+            echo '<div id="article-footer">';
+            echo '<div style="width: 50%; height: 30px; float: left; margin-bottom: 2px; font-size:11px; line-height: 30px;">';
+            echo __('Categories', 'agora-functions') . implode(" | ", $allCategories);
+            echo '</div>';
 
-                     	if (get_comments_number()) {
-                     		echo '<div style="width: 40%; float: right; color: #1fa799 !important;">';
-                     			echo '<span style="float: right; margin: 0 0 0 7px; font-size: 26px; font-weight: bold; line-height: 1; text-shadow: 0 1px 0 white; font-style: italic;">' . get_comments_number(). '</span>';
-                     			echo '<span style="font-size:11px; line-height: 28px; float: right; margin: 0 0 0 7px; font: italic 11px "Droid Serif",Georgia,"Times New Roman",Times,serif;">';
-                     				echo __('Comments', 'agora-functions');
-	                     		echo "</span>";
-	                     	echo '</div>';
-                     	}
-                     echo '</div>';
+            if (get_comments_number()) {
+                echo '<div style="width: 40%; float: right; color: #1fa799 !important;">';
+                echo '<span style="float: right; margin: 0 0 0 7px; font-size: 26px; font-weight: bold; line-height: 1; text-shadow: 0 1px 0 white; font-style: italic;">' . get_comments_number() . '</span>';
+                echo '<span style="font-size:11px; line-height: 28px; float: right; margin: 0 0 0 7px; font: italic 11px "Droid Serif",Georgia,"Times New Roman",Times,serif;">';
+                echo __('Comments', 'agora-functions');
+                echo "</span>";
+                echo '</div>';
+            }
+            echo '</div>';
 
-                     echo '<div style="clear:both"></div>';
+            echo '<div style="clear:both"></div>';
 
-				echo '</div>';
-			echo '</div>';
-		};
+            echo '</div>';
+            echo '</div>';
+        }
+    } else {
+        echo __('Article not found', 'agora-functions');
+    }
 
-	} else {
-		echo __('Article not found', 'agora-functions');
-	};
-
-	//Reset Query
-	wp_reset_query();
+    //Reset Query
+    wp_reset_query();
 }
 
 /**
  * Return the post number from a user
  * @author Nacho Abejaro
  */
-function get_user_posts_count($status){
-	$args = array();
-	$args['post_status'] = $status;
-	$args['author'] = bp_displayed_user_id();
-	$args['fields'] = 'ids';
-	$args['posts_per_page'] = "-1";
-	$args['post_type'] = 'post';
-	$ps = get_posts($args);
-	return count($ps);
+function get_user_posts_count($status) {
+    $args = array();
+    $args['post_status'] = $status;
+    $args['author'] = bp_displayed_user_id();
+    $args['fields'] = 'ids';
+    $args['posts_per_page'] = "-1";
+    $args['post_type'] = 'post';
+    $ps = get_posts($args);
+    return count($ps);
 }
 
 /**
@@ -226,14 +229,14 @@ function get_user_posts_count($status){
  * @author Nacho Abejaro
  */
 function limit_text($text, $limitwrd) {
-	if (str_word_count($text) > $limitwrd) {
-		$words = str_word_count($text, 2);
-		if ($words > $limitwrd) {
-			$pos = array_keys($words);
-			$text = substr($text, 0, $pos[$limitwrd]) . ' [...]';
-		}
-	}
-	return $text;
+    if (str_word_count($text) > $limitwrd) {
+        $words = str_word_count($text, 2);
+        if ($words > $limitwrd) {
+            $pos = array_keys($words);
+            $text = substr($text, 0, $pos[$limitwrd]) . ' [...]';
+        }
+    }
+    return $text;
 }
 
 /**
@@ -241,14 +244,15 @@ function limit_text($text, $limitwrd) {
  * @author Nacho Abejaro
  */
 function members_logged_in_only() {
-	$uid = get_current_user_id();
+    $uid = get_current_user_id();
 
-	if ($uid == 0) {
-		//Instead of using wp_redirect, echo location using meta
-		$location = home_url()."/wp-login.php";
-		echo "<meta http-equiv='refresh' content='0;url=$location'/>";
-	}
+    if ($uid == 0) {
+        //Instead of using wp_redirect, echo location using meta
+        $location = home_url() . "/wp-login.php";
+        echo "<meta http-equiv='refresh' content='0;url=$location'/>";
+    }
 }
+
 add_filter('bp_before_member_home_content', 'members_logged_in_only');
 
 /**
@@ -262,9 +266,10 @@ add_filter( 'bp_core_fetch_avatar_no_grav', '__return_true' );
  * @author Sara Arjona
  */
 function agora_remove_post_meta_boxes() {
-	remove_meta_box('formatdiv', 'post', 'normal');
-	remove_meta_box('formatdiv', 'post', 'side');
+    remove_meta_box('formatdiv', 'post', 'normal');
+    remove_meta_box('formatdiv', 'post', 'side');
 }
+
 add_action('do_meta_boxes', 'agora_remove_post_meta_boxes');
 
 /**
@@ -273,34 +278,33 @@ add_action('do_meta_boxes', 'agora_remove_post_meta_boxes');
  */
 remove_filter('get_the_excerpt', 'wp_trim_excerpt');
 add_filter('get_the_excerpt', 'preserve_excerpt_format');
+
 function preserve_excerpt_format($text) {
-	global $post;
-	$raw_excerpt = $text;
-	if ('' == $text )
-	{
-		$text = get_the_content('');
-		$text = strip_shortcodes($text);
-		$text = apply_filters('the_content', $text);
-		$text = str_replace(']]>', ']]&gt;', $text);
+    //global $post;
+    $raw_excerpt = $text;
+    if ('' == $text) {
+        $text = get_the_content('');
+        $text = strip_shortcodes($text);
+        $text = apply_filters('the_content', $text);
+        $text = str_replace(']]>', ']]&gt;', $text);
 
-		$exceptions = '<p>,<a>,<em>,<strong>,<br><img>'; //PRESERVE THESE TAGS, ADD/REMOVE AS NEEDED
-		$text = strip_tags($text, $exceptions);
+        $exceptions = '<p>,<a>,<em>,<strong>,<br><img>'; //PRESERVE THESE TAGS, ADD/REMOVE AS NEEDED
+        $text = strip_tags($text, $exceptions);
 
-		$maxCount = 55; //DEFAULT WP WORD COUNT, INCREASE AS NEEDED
-		$excerpt_length = apply_filters('excerpt_length', $maxCount);
+        $maxCount = 55; //DEFAULT WP WORD COUNT, INCREASE AS NEEDED
+        $excerpt_length = apply_filters('excerpt_length', $maxCount);
 
-		//$moreText = '.... <a class="blue" href="'.get_permalink($post->ID).'">Read More &gt;&gt;</a>'; //CUSTOM MORE TEXT, CHANGE AS NEEDED
-		$excerpt_more = apply_filters('excerpt_more', '');
+        //$moreText = '.... <a class="blue" href="'.get_permalink($post->ID).'">Read More &gt;&gt;</a>'; //CUSTOM MORE TEXT, CHANGE AS NEEDED
+        $excerpt_more = apply_filters('excerpt_more', '');
 
-		$words = preg_split("/[\n\r\t\ ]+/", $text, $excerpt_length+1, PREG_SPLIT_NO_EMPTY);
-		if(count($words) > $excerpt_length)
-		{
-			array_pop($words);
-			$text = implode(' ', $words);
-			//$text = $text.$excerpt_more;
-		}
-		else
-			$text = implode(' ', $words);
-	}
-	return apply_filters('wp_trim_excerpt', $text, $raw_excerpt);
+        $words = preg_split("/[\n\r\t\ ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY);
+        if (count($words) > $excerpt_length) {
+            array_pop($words);
+            $text = implode(' ', $words);
+            //$text = $text.$excerpt_more;
+        } else {
+            $text = implode(' ', $words);
+        }
+    }
+    return apply_filters('wp_trim_excerpt', $text, $raw_excerpt);
 }
