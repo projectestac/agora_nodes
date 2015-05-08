ca721e96055e1448ebe1af6ecd1ef69231b5fe2e
Toni Ginard <toni.ginard@gmail.com>
2015-05-08 14:40:32 +0200

    Synced version of WSL from XTECBlocs

--- wp-content/plugins/wordpress-social-login/includes/admin/wsl.admin.ui.php
+++ wp-content/plugins/wordpress-social-login/includes/admin/wsl.admin.ui.php
@@ -6,7 +6,7 @@
 *  (c) 2011-2014 Mohamed Mrassi and contributors | http://wordpress.org/plugins/wordpress-social-login/
 */
 
-/** 
+/**
 * The LOC in charge of displaying WSL Admin GUInterfaces
 */
 
@@ -16,13 +16,13 @@ if ( !defined( 'ABSPATH' ) ) exit;
 // --------------------------------------------------------------------
 
 /**
-* Generate wsl admin pages 
+* Generate wsl admin pages
 *
-* wp-admin/options-general.php?page=wordpress-social-login&.. 
+* wp-admin/options-general.php?page=wordpress-social-login&..
 */
 function wsl_admin_main()
 {
-	// HOOKABLE: 
+	// HOOKABLE:
 	do_action( "wsl_admin_main_start" );
 
 	if ( ! current_user_can('manage_options') )
@@ -67,7 +67,7 @@ function wsl_admin_main()
 // XTEC ************ MODIFICAT - Hide tabs "Widget" and "Components"
 // 2014.11.14 @aginard
     global $isAgora;
-    
+
     if ($isAgora) {
         $wslp        = "users";
     } else {
@@ -92,12 +92,12 @@ function wsl_admin_main()
 	if( isset( $WORDPRESS_SOCIAL_LOGIN_ADMIN_TABS[$wslp] ) && $WORDPRESS_SOCIAL_LOGIN_ADMIN_TABS[$wslp]["enabled"] )
 	{
 		if( isset( $WORDPRESS_SOCIAL_LOGIN_ADMIN_TABS[$wslp]["action"] ) && $WORDPRESS_SOCIAL_LOGIN_ADMIN_TABS[$wslp]["action"] )
-		{ 
+		{
 			do_action( $WORDPRESS_SOCIAL_LOGIN_ADMIN_TABS[$wslp]["action"] );
 		}
 		else
 		{
-			include "components/$wslp/index.php"; 
+			include "components/$wslp/index.php";
 		}
 	}
 	else
@@ -107,7 +107,7 @@ function wsl_admin_main()
 
 	wsl_admin_ui_footer();
 
-	// HOOKABLE: 
+	// HOOKABLE:
 	do_action( "wsl_admin_main_end" );
 }
 
@@ -118,28 +118,28 @@ function wsl_admin_main()
 */
 function wsl_admin_ui_header( $wslp = null )
 {
-	// HOOKABLE: 
+	// HOOKABLE:
 	do_action( "wsl_admin_ui_header_start" );
 
 	GLOBAL $WORDPRESS_SOCIAL_LOGIN_VERSION;
 	GLOBAL $WORDPRESS_SOCIAL_LOGIN_ADMIN_TABS;
-	
+
 ?>
 <a name="wsltop"></a>
 <div class="wsl-container">
 
 	<?php
 		// nag
-	
+
 		if( in_array( $wslp, array( 'networks', 'login-widget' ) ) and ( isset( $_REQUEST['settings-updated'] ) or isset( $_REQUEST['enable'] ) ) )
 		{
-			$active_plugins = implode('', (array) get_option('active_plugins') ); 
-			$cache_enabled  = 
-				strpos( $active_plugins, "w3-total-cache"   ) !== false | 
-				strpos( $active_plugins, "wp-super-cache"   ) !== false | 
-				strpos( $active_plugins, "quick-cache"      ) !== false | 
-				strpos( $active_plugins, "wp-fastest-cache" ) !== false | 
-				strpos( $active_plugins, "wp-widget-cache"  ) !== false | 
+			$active_plugins = implode('', (array) get_option('active_plugins') );
+			$cache_enabled  =
+				strpos( $active_plugins, "w3-total-cache"   ) !== false |
+				strpos( $active_plugins, "wp-super-cache"   ) !== false |
+				strpos( $active_plugins, "quick-cache"      ) !== false |
+				strpos( $active_plugins, "wp-fastest-cache" ) !== false |
+				strpos( $active_plugins, "wp-widget-cache"  ) !== false |
 				strpos( $active_plugins, "hyper-cache"      ) !== false;
 
 			if( $cache_enabled )
@@ -153,7 +153,7 @@ function wsl_admin_ui_header( $wslp = null )
 				<?php
 			}
 		}
-		
+
 		if( get_option( 'wsl_settings_development_mode_enabled' ) )
 		{
 			?>
@@ -187,8 +187,8 @@ function wsl_admin_ui_header( $wslp = null )
 	?>
 
 	<div class="alignright">
-		<a style="font-size: 0.9em; text-decoration: none;" target="_blank" href="http://miled.github.io/wordpress-social-login/documentation.html"><?php _wsl_e('Docs', 'wordpress-social-login') ?></a> - 
-		<a style="font-size: 0.9em; text-decoration: none;" target="_blank" href="http://miled.github.io/wordpress-social-login/support.html"><?php _wsl_e('Support', 'wordpress-social-login') ?></a> - 
+		<a style="font-size: 0.9em; text-decoration: none;" target="_blank" href="http://miled.github.io/wordpress-social-login/documentation.html"><?php _wsl_e('Docs', 'wordpress-social-login') ?></a> -
+		<a style="font-size: 0.9em; text-decoration: none;" target="_blank" href="http://miled.github.io/wordpress-social-login/support.html"><?php _wsl_e('Support', 'wordpress-social-login') ?></a> -
 		<a style="font-size: 0.9em; text-decoration: none;" target="_blank" href="https://github.com/miled/wordpress-social-login"><?php _wsl_e('Github', 'wordpress-social-login') ?></a>
 	</div>
 
@@ -225,7 +225,7 @@ function wsl_admin_ui_header( $wslp = null )
 
 	<div id="wsl_admin_tab_content">
 <?php
-	// HOOKABLE: 
+	// HOOKABLE:
 	do_action( "wsl_admin_ui_header_end" );
 }
 
@@ -236,23 +236,23 @@ function wsl_admin_ui_header( $wslp = null )
 */
 function wsl_admin_ui_footer()
 {
-	// HOOKABLE: 
+	// HOOKABLE:
 	do_action( "wsl_admin_ui_footer_start" );
 
 	GLOBAL $WORDPRESS_SOCIAL_LOGIN_VERSION;
 ?>
-	</div> <!-- ./wsl_admin_tab_content -->  
-	
+	</div> <!-- ./wsl_admin_tab_content -->
+
 <div class="clear"></div>
 
 <?php
 	wsl_admin_help_us_localize_note();
 
-	// HOOKABLE: 
+	// HOOKABLE:
 	do_action( "wsl_admin_ui_footer_end" );
 
 	if( get_option( 'wsl_settings_development_mode_enabled' ) )
-	{ 
+	{
 		wsl_display_dev_mode_debugging_area();
  	}
 }
@@ -264,11 +264,11 @@ function wsl_admin_ui_footer()
 */
 function wsl_admin_ui_error()
 {
-	// HOOKABLE: 
+	// HOOKABLE:
 	do_action( "wsl_admin_ui_error_start" );
-?> 
+?>
 <div id="wsl_div_warn">
-	<h3 style="margin:0px;"><?php _wsl_e('Oops! We ran into an issue.', 'wordpress-social-login') ?></h3> 
+	<h3 style="margin:0px;"><?php _wsl_e('Oops! We ran into an issue.', 'wordpress-social-login') ?></h3>
 
 	<hr />
 
@@ -285,10 +285,10 @@ function wsl_admin_ui_error()
 	<div>
 		<a class="button-secondary" href="http://miled.github.io/wordpress-social-login/support.html" target="_blank"><?php _wsl_e( "Report as bug", 'wordpress-social-login' ) ?></a>
 		<a class="button-primary" href="options-general.php?page=wordpress-social-login&wslp=components" style="float:<?php if( is_rtl() ) echo 'left'; else echo 'right'; ?>"><?php _wsl_e( "Check enabled components", 'wordpress-social-login' ) ?></a>
-	</div> 
-</div>  
+	</div>
+</div>
 <?php
-	// HOOKABLE: 
+	// HOOKABLE:
 	do_action( "wsl_admin_ui_error_end" );
 }
 
@@ -299,7 +299,7 @@ function wsl_admin_ui_error()
 */
 function wsl_admin_ui_fail()
 {
-	// HOOKABLE: 
+	// HOOKABLE:
 	do_action( "wsl_admin_ui_fail_start" );
 ?>
 <div class="wsl-container">
@@ -308,27 +308,27 @@ function wsl_admin_ui_fail()
 
 			<hr />
 
-			<p> 
-				<?php _e('Despite the efforts, put into <b>WordPress Social Login</b> in terms of reliability, portability, and maintenance by the plugin <a href="http://profiles.wordpress.org/miled/" target="_blank">author</a> and <a href="https://github.com/hybridauth/WordPress-Social-Login/graphs/contributors" target="_blank">contributors</a>', 'wordpress-social-login') ?>. 
+			<p>
+				<?php _e('Despite the efforts, put into <b>WordPress Social Login</b> in terms of reliability, portability, and maintenance by the plugin <a href="http://profiles.wordpress.org/miled/" target="_blank">author</a> and <a href="https://github.com/hybridauth/WordPress-Social-Login/graphs/contributors" target="_blank">contributors</a>', 'wordpress-social-login') ?>.
 				<b style="color:red;"><?php _e('Your server failed the requirements check for this plugin', 'wordpress-social-login') ?>:</b>
-			</p> 
+			</p>
 
-			<p> 
+			<p>
 				<?php _e('These requirements are usually met by default by most "modern" web hosting providers, however some complications may occur with <b>shared hosting</b> and, or <b>custom wordpress installations</b>', 'wordpress-social-login') ?>.
-			</p> 
+			</p>
 
 			<p>
 				<?php _wsl_e("The minimum server requirements are", 'wordpress-social-login') ?>:
 			</p>
 
 			<ul style="margin-left:60px;">
-				<li><?php _wsl_e("PHP >= 5.2.0 installed", 'wordpress-social-login') ?></li> 
+				<li><?php _wsl_e("PHP >= 5.2.0 installed", 'wordpress-social-login') ?></li>
 				<li><?php _wsl_e("WSL Endpoint URLs reachable", 'wordpress-social-login') ?></li>
 				<li><?php _wsl_e("PHP's default SESSION handling", 'wordpress-social-login') ?></li>
-				<li><?php _wsl_e("PHP/CURL/SSL Extension enabled", 'wordpress-social-login') ?></li> 
-				<li><?php _wsl_e("PHP/JSON Extension enabled", 'wordpress-social-login') ?></li> 
-				<li><?php _wsl_e("PHP/REGISTER_GLOBALS Off", 'wordpress-social-login') ?></li> 
-				<li><?php _wsl_e("jQuery installed on WordPress backoffice", 'wordpress-social-login') ?></li> 
+				<li><?php _wsl_e("PHP/CURL/SSL Extension enabled", 'wordpress-social-login') ?></li>
+				<li><?php _wsl_e("PHP/JSON Extension enabled", 'wordpress-social-login') ?></li>
+				<li><?php _wsl_e("PHP/REGISTER_GLOBALS Off", 'wordpress-social-login') ?></li>
+				<li><?php _wsl_e("jQuery installed on WordPress backoffice", 'wordpress-social-login') ?></li>
 			</ul>
 		</div>
 
@@ -340,7 +340,7 @@ function wsl_admin_ui_fail()
 </div>
 <style>.wsl-container .button-secondary { display:none; }</style>
 <?php
-	// HOOKABLE: 
+	// HOOKABLE:
 	do_action( "wsl_admin_ui_fail_end" );
 }
 
@@ -362,18 +362,18 @@ function wsl_admin_welcome_panel()
 
 	// if new user or wsl updated, then we display wsl welcome panel
 	if( get_option( 'wsl_settings_welcome_panel_enabled' ) == wsl_get_version() )
-	{ 
+	{
 		return;
 	}
-	
-	$wslp = "networks"; 
+
+	$wslp = "networks";
 
 	if( isset( $_REQUEST["wslp"] ) )
 	{
 		$wslp = $_REQUEST["wslp"];
-	}	
-?> 
-<!-- 
+	}
+?>
+<!--
 	if you want to know if a UI was made by developer, then here is a tip: he will always use tables
 
 	//> wsl-w-panel is shamelessly borrowed and modified from wordpress welcome-panel
@@ -383,14 +383,14 @@ function wsl_admin_welcome_panel()
 <!-- 2014.11.14 @aginard -->
 <?php
     global $isAgora, $isBlocs;
-    if (($isAgora && !is_xtecadmin()) && !$isBlocs) {
+    if (($isAgora && !is_xtecadmin()) || $isBlocs) {
         echo '<!--';
     }
 //************ FI (The following close PHP tag belongs to the patch) ?>
 
 <div id="wsl-w-panel">
 	<a href="options-general.php?page=wordpress-social-login&wslp=<?php echo $wslp ?>&wsldwp=1" id="wsl-w-panel-dismiss" <?php if( is_rtl() ) echo 'style="left: 10px;right: auto;"'; ?>><?php _wsl_e("Dismiss", 'wordpress-social-login') ?></a>
-	
+
 	<table width="100%" border="0" style="margin:0;padding:0;">
 		<tr>
 			<td width="10" valign="top"></td>
@@ -410,20 +410,20 @@ function wsl_admin_welcome_panel()
 					<li><a href="http://miled.github.io/wordpress-social-login/overview.html" target="_blank"><?php _wsl_e('Plugin Overview', 'wordpress-social-login') ?></a></li>
 					<li><a href="http://miled.github.io/wordpress-social-login/networks.html" target="_blank"><?php _wsl_e('Setup and Configuration', 'wordpress-social-login') ?></a></li>
 					<li><a href="http://miled.github.io/wordpress-social-login/widget.html" target="_blank"><?php _wsl_e('Customize WSL Widgets', 'wordpress-social-login') ?></a></li>
-					<li><a href="http://miled.github.io/wordpress-social-login/userdata.html" target="_blank"><?php _wsl_e('Manage users and contacts', 'wordpress-social-login') ?></a></li> 
-					<li><a href="http://miled.github.io/wordpress-social-login/documentation.html" target="_blank"><?php _wsl_e('WSL Developer API', 'wordpress-social-login') ?></a></li> 
+					<li><a href="http://miled.github.io/wordpress-social-login/userdata.html" target="_blank"><?php _wsl_e('Manage users and contacts', 'wordpress-social-login') ?></a></li>
+					<li><a href="http://miled.github.io/wordpress-social-login/documentation.html" target="_blank"><?php _wsl_e('WSL Developer API', 'wordpress-social-login') ?></a></li>
 				</ul>
 			</td>
 			<td width="" valign="top">
 				<br />
 				<p>
-					<b><?php echo sprintf( _wsl__( "What's new on WSL %s", 'wordpress-social-login'), wsl_get_version() ) ?></b>
+					<b><?php  echo sprintf( _wsl__( "What's new on WSL %s", 'wordpress-social-login'), wsl_get_version() ) ?></b>
 				</p>
 
 				<ul style="margin-left:25px;">
-					<li><?php _wsl_e('WSL now support authentications through <a href="https://dribbble.com" target="_blank">Dribbble.com</a>. Hooray for fellow designers', 'wordpress-social-login') ?>!</li> 
-					<li><?php _wsl_e('<a href="http://store.steampowered.com/" target="_blank">Steam</a> provider has been entirely reworked and now fully support the new Web API', 'wordpress-social-login') ?>.</li> 
-					<li><?php _wsl_e('WSL admin interfaces have been reworked and can be now extended with hooks', 'wordpress-social-login') ?>.</li> 
+					<li><?php _wsl_e('WSL now support authentications through <a href="https://dribbble.com" target="_blank">Dribbble.com</a>. Hooray for fellow designers', 'wordpress-social-login') ?>!</li>
+					<li><?php _wsl_e('<a href="http://store.steampowered.com/" target="_blank">Steam</a> provider has been entirely reworked and now fully support the new Web API', 'wordpress-social-login') ?>.</li>
+					<li><?php _wsl_e('WSL admin interfaces have been reworked and can be now extended with hooks', 'wordpress-social-login') ?>.</li>
 					<li><?php _wsl_e('Profile completion form has received a visual update', 'wordpress-social-login') ?>.</li>
 					<li><?php _wsl_e('Bouncer Membership level can be now set to any user role', 'wordpress-social-login') ?>.</li>
 					<li><?php _wsl_e('WSL now provide an <a href="http://miled.github.io/wordpress-social-login/developer-api-apis.html" target="_blank">easier access</a> to social networks apis', 'wordpress-social-login') ?>.</li>
@@ -436,17 +436,17 @@ function wsl_admin_welcome_panel()
 				&nbsp;
 			</td>
 		</tr>
-	</table> 
+	</table>
 </div>
-<?php 
+<?php
 
 // XTEC ************ AFEGIT - Remove Welcome banner
 // 2014.11.14 @aginard
-    if (($isAgora && !is_xtecadmin()) && !$isBlocs) {
+    if (($isAgora && !is_xtecadmin()) || $isBlocs) {
         echo '-->';
     }
 //************ FI
-    
+
 }
 
 // --------------------------------------------------------------------
@@ -456,11 +456,11 @@ function wsl_admin_welcome_panel()
 */
 function wsl_admin_help_us_localize_note()
 {
-	return; // nothing, until I decide otherwise.. 
+	return; // nothing, until I decide otherwise..
 
-	$assets_url = WORDPRESS_SOCIAL_LOGIN_PLUGIN_URL . '/assets/img/'; 
+	$assets_url = WORDPRESS_SOCIAL_LOGIN_PLUGIN_URL . '/assets/img/';
 
-	?> 
+	?>
 		<div id="l10n-footer">
 			<br /><br />
 			<img src="<?php echo $assets_url ?>flags.png">
@@ -482,20 +482,20 @@ function wsl_render_wp_editor( $name, $content )
 	if( ! function_exists( 'wp_editor' ) )
 	{
 		?>
-			<textarea style="width:100%;height:100px;margin-top:6px;" name="<?php echo $name ?>"><?php echo htmlentities( $content ); ?></textarea> 
+			<textarea style="width:100%;height:100px;margin-top:6px;" name="<?php echo $name ?>"><?php echo htmlentities( $content ); ?></textarea>
 		<?php
 		return;
 	}
 ?>
-<div class="postbox"> 
+<div class="postbox">
 	<div class="wp-editor-textarea" style="background-color: #FFFFFF;">
-		<?php 
-			wp_editor( 
-				$content, $name, 
-				array( 'textarea_name' => $name, 'media_buttons' => true, 'tinymce' => array( 'theme_advanced_buttons1' => 'formatselect,forecolor,|,bold,italic,underline,|,justifyleft,justifycenter,justifyright,justifyfull,|,link,unlink' ) ) 
+		<?php
+			wp_editor(
+				$content, $name,
+				array( 'textarea_name' => $name, 'media_buttons' => true, 'tinymce' => array( 'theme_advanced_buttons1' => 'formatselect,forecolor,|,bold,italic,underline,|,justifyleft,justifycenter,justifyright,justifyfull,|,link,unlink' ) )
 			);
 		?>
-	</div> 
+	</div>
 </div>
 <?php
 }
@@ -503,7 +503,7 @@ function wsl_render_wp_editor( $name, $content )
 // --------------------------------------------------------------------
 
 /**
-* Display WordPress Social Login on settings as submenu 
+* Display WordPress Social Login on settings as submenu
 */
 function wsl_admin_menu()
 {
@@ -512,7 +512,7 @@ function wsl_admin_menu()
 	add_action( 'admin_init', 'wsl_register_setting' );
 }
 
-add_action('admin_menu', 'wsl_admin_menu' ); 
+add_action('admin_menu', 'wsl_admin_menu' );
 
 // --------------------------------------------------------------------
 
@@ -523,10 +523,10 @@ function wsl_add_admin_stylesheets()
 {
 	if( ! wp_style_is( 'wsl-admin', 'registered' ) )
 	{
-		wp_register_style( "wsl-admin", WORDPRESS_SOCIAL_LOGIN_PLUGIN_URL . "/assets/css/admin.css" ); 
+		wp_register_style( "wsl-admin", WORDPRESS_SOCIAL_LOGIN_PLUGIN_URL . "/assets/css/admin.css" );
 	}
 
-	wp_enqueue_style( "wsl-admin" ); 
+	wp_enqueue_style( "wsl-admin" );
 }
 
 add_action( 'admin_enqueue_scripts', 'wsl_add_admin_stylesheets' );
