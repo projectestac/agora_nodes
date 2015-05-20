3d04e40ad1bd0f07f4cd271b6282f55fadc37f2e
Toni Ginard <toni.ginard@gmail.com>
2015-05-20 14:28:18 +0200

    Fixed upgrade script not detecting versions correctly

--- wp-includes/xtec/scripts/script_upgrade.class.php
+++ wp-includes/xtec/scripts/script_upgrade.class.php
@@ -8,14 +8,19 @@ class script_upgrade extends agora_script_base {
     public $info = "Crida l'script wp-admin/upgrade.php de cada espai per dur a terme l'actualització estàndard del WordPress";
 
     protected function _execute($params = array()) {
+        
+        global $wp_db_version;
 
-        if (get_option('db_version') == $wp_db_version || !is_blog_installed()) {
+        echo 'Versió de la base dades: ' . get_option('db_version') . '<br />';
+        echo 'Versió dels fitxers: ' . $wp_db_version . '<br />';
+
+        if ((get_option('db_version') != $wp_db_version) || !is_blog_installed()) {
             $_GET['step'] = 1;
             require_once ABSPATH . 'wp-admin/upgrade.php';
         } else {
-            echo "<br />";
-            echo "El WordPress ja estava actualitzat!";
-            echo "<br />";
+            echo '<br />';
+            echo 'El WordPress ja estava actualitzat!';
+            echo '<br />';
         }
 
         return true;
