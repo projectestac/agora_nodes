fa2c6513695383103ded3a188031b56ca52b15cd
Toni Ginard <toni.ginard@gmail.com>
2015-06-12 17:15:25 +0200

    Removed plugin Social Articles

--- wp-content/plugins/social-articles/includes/templates/members/single/articles/pending.php
+++
@@ -1,9 +0,0 @@
-<?php global $bp, $post, $wpdb, $socialArticles;
-?>
-<section id="articles-container">
-    <?php if(bp_displayed_user_id()==bp_loggedin_user_id()):?>
-        <div class="articles-container">
-            <?php get_articles(0, 'pending', true);?>
-        </div>
-    <?php endif;?>
-</section>
