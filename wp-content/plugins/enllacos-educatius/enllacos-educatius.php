c70fa82fab80ad5b7cd4a10ade978069eec8faf1
Xavi Meler <jmeler@xtec.cat>
2015-08-19 10:08:05 +0200

    Enllaços educatius: Updated URL of edu3 (GitHub #225)

--- wp-content/plugins/enllacos-educatius/enllacos-educatius.php
+++ wp-content/plugins/enllacos-educatius/enllacos-educatius.php
@@ -20,7 +20,7 @@ class XTEC_Widget extends WP_Widget {
         'ensenyament' => ['nom' => "Dep.Ensenyament", 'url' => 'http://www20.gencat.cat/portal/site/ensenyament', 'img' => 'ensenyament-icon.png', 'desc' => 'Pàgina del Departament d\'ensenyament'],
         'xtec' => ['nom' => "XTEC", 'url' => 'http://xtec.cat', 'img' => 'xtec-icon.png', 'desc' => 'Recursos educatius'],
         'edu365' => ['nom' => "Edu365", 'url' => 'http://edu365.cat', 'img' => 'edu365-icon.png', 'desc' => 'Recursos educatius'],
-        'edu3' => ['nom' => "Edu3", 'url' => 'http://edu3.cat', 'img' => 'edu3-icon.png', 'desc' => 'Videos educatius'],
+        'edu3' => ['nom' => "Edu3", 'url' => 'http://www.edu3.cat', 'img' => 'edu3-icon.png', 'desc' => 'Videos educatius'],
         'xarxa-docent' => ['nom' => "Xarxa Docent", 'url' => 'http://educat.xtec.cat', 'img' => 'xarxa-docent-icon.png', 'desc' => 'Xarxa de support amb més de 10.000 docents inscrits'],
         'ateneu' => ['nom' => "Ateneu", 'url' => 'http://ateneu.xtec.cat/wikiform/wikiexport/cursos/index', 'img' => 'ateneu-icon.png', 'desc' => 'Materials i recursos per la formació'],
         'alexandria' => ['nom' => "Alexandria", 'url' => 'http://alexandria.xtec.cat', 'img' => 'alexandria-icon.png', 'desc' => 'Cursos moodle i activitats PDI per descarregar'],
