dcafc1dc636975a6e8d791d77602b1c1bb9949d0
ignacio.benito.abejaro@upcnet.es <ignacio.benito.abejaro@upcnet.es>
2015-11-10 17:40:26 +0100

    Theme reactor-primaria-1: Fixed side menu in pages when there are subpages

--- wp-content/themes/reactor/library/js/reactor.js
+++ wp-content/themes/reactor/library/js/reactor.js
@@ -1,34 +1,79 @@
 /* Reactor - Anthony Wilhelm - http://awtheme.com/ */
 ( function($) {
+	$(document).ready( function() {
+		/* adds .button class to submit button on comment form */
+		$('#commentform input#submit').addClass('button').addClass('small');
 
-  $(document).ready( function() {
-	
-    /* adds .button class to submit button on comment form */
-    $('#commentform input#submit').addClass('button').addClass('small');
-  
-    /* adjust site for fixed top-bar with wp admin bar */
-    if($('body').hasClass('admin-bar')) {
-    	if($('.top-bar').parent().hasClass('fixed')) {
-    		
-		if($('body').hasClass('has-top-bar')) {
-	    	    $('.top-bar').parent().css('margin-top', "+=28");
+		/* adjust site for fixed top-bar with wp admin bar */
+		if($('body').hasClass('admin-bar')) {
+			if($('.top-bar').parent().hasClass('fixed')) {
+				if($('body').hasClass('has-top-bar')) {
+					$('.top-bar').parent().css('margin-top', "+=28");
+				}
+				$('body').css('padding-top', "+=28");
+			}
 		}
-		
-		$('body').css('padding-top', "+=28");
-	}
+
+	/* prevent default if menu links are # */
+	$('nav a').each(function() {
+		var nav = $(this);
+		if(nav.attr('href') == '#') {
+			$(this).on('click', function(e){
+				e.preventDefault();
+			});
+		}
+	});
+
+	/**
+	* // XTEC ************ AFEGIT
+	* SideMenuWalker: Search all div class="section" when document is ready,
+    * and check if the section have not childrens (div="content"). If not found any children,
+    * class "noChildren" is added for no load the up arrow image
+	* 2015.11.13 @nacho
+    */
+	var SideMenuWalker = {
+		init: function() {
+			var sections = $('div.section');
+			var content = sections.find('div.content');
+			sections.each(function(index, value){
+				var self = jQuery(this);
+				var content = self.find('div.content');
+				if (!content.length) {
+					self.addClass("noChildren");
+				}
+			});
+		}
     }
+	SideMenuWalker.init();
 
-    /* prevent default if menu links are # */
-    $('nav a').each(function() {
-        var nav = $(this); 
-        if(nav.attr('href') == '#') {
-            $(this).on('click', function(e){ 
-                e.preventDefault();
-            });
-        }
-    });
+	/**
+     * Control the click event on dropDown class to add or remove a parent class
+    */
+	$('.dropDown').on('click', function(e) {
+		e.preventDefault();
+		var section = $(this).parent().closest('div');
 
-    /* MixItUp - http://mixitup.io/ */
+		if (section.hasClass('section')){
+			section.removeClass("section");
+			section.addClass("customSection active");
+		}else {
+			section.removeClass("customSection active");
+			section.addClass("section");
+		}
+	});
+
+	/**
+     * Control the click event on a section for load the content through the correct link
+     */
+	$('.section a').on('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		var href = $(this);
+		window.location.href = href.attr('href');
+	});
+	//************ FI
+
+	/* MixItUp - http://mixitup.io/ */
     if($().mixitup) {
         $(function(){
             $('#Grid').mixitup();
@@ -49,5 +94,4 @@
 	
 	/* Initialize Foundation Scripts */
 	$(document).foundation();
-
-})( jQuery );	
+})( jQuery );
