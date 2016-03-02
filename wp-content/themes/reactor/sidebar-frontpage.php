3636e405c1edd13be16ae5e28df4ae33c22ee11d
Pau Ferrer Ocaña <crazyserver@gmail.com>
2016-03-02 16:46:24 +0100

    Themes: Revert changes made to avoid 3 columns

--- wp-content/themes/reactor/sidebar-frontpage.php
+++ wp-content/themes/reactor/sidebar-frontpage.php
@@ -7,8 +7,11 @@
  * @since 1.0.0
  */
 ?>
-<?php reactor_sidebar_before(); ?>
-<div id="sidebar-frontpage" class="sidebar <?php reactor_columns( 3 , true, false, null, -9); ?>" role="complementary">
+<?php reactor_sidebar_before();
+$frontpage_layout = reactor_option('frontpage_layout');
+$pull = $frontpage_layout == '2c-r' || $frontpage_layout == '3c-c' ? -6 : -9;
+?>
+<div id="sidebar-frontpage" class="sidebar <?php reactor_columns( 3 , true, false, null, $pull); ?>" role="complementary">
     <div id="sidebar-frontpage-inner">
         <?php dynamic_sidebar('sidebar-frontpage'); ?>
     </div>
