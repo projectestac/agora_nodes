5f3797b232585af1d3bb3086aa7709f7f57eb6d0
Pau Ferrer Ocaña <crazyserver@gmail.com>
2016-03-03 12:51:48 +0100

    Themes: Merge side-menu template

--- wp-content/themes/reactor/loops/loop-taxonomy.php
+++ wp-content/themes/reactor/loops/loop-taxonomy.php
@@ -9,53 +9,75 @@
 ?>
 
 <?php
-global $card_colors;
 global $card_bgcolor;
-global $layout;
+global $column_amplada;
 global $posts_per_fila1;
 global $posts_per_fila2;
 global $posts_per_filan;
 
-$card_colors=array("card_bgcolor1","card_bgcolor2","card_bgcolor3");
-$rows=array(1=>$posts_per_fila1,2=>$posts_per_fila2,3=>$posts_per_filan);
-$aLayout=array( 1=>array(),2=>array(),3=>array());
+$card_colors = array("card_bgcolor1", "card_bgcolor2", "card_bgcolor3");
+$rows = array ($posts_per_fila1, $posts_per_fila2, $posts_per_filan);
+$aLayout = array(array(), array(), array());
 
-foreach ($rows as $row=>$posts_per_fila ) {			 	
+foreach ($rows as $row => $posts_per_fila) {
     switch ($posts_per_fila) {
-        case 1: array_push($aLayout[$row],1);
-         break;
-        case 2: array_push($aLayout[$row],2,2);
-         break;
-        case 3: array_push($aLayout[$row],3,3,3);
-         break;
-        case 4: array_push($aLayout[$row],4,4,4,4);
-         break;
-        case 33: array_push($aLayout[$row],3,66);
-         break;
-        case 66: array_push($aLayout[$row],66,3);
-         break;
-        default: array_push($aLayout[$row],2,2);
+        case 1:
+            $aLayout[$row] = array(12);
+            break;
+        case 3:
+            $aLayout[$row] = array(4, 4, 4);
+            break;
+        case 4:
+            $aLayout[$row] = array(3, 3, 3, 3);
+            break;
+        case 33:
+            $aLayout[$row] = array(4, 8);
+            break;
+        case 66:
+            $aLayout[$row] = array(8, 4);
+            break;
+        case 2:
+        default:
+            $aLayout[$row] = array(6, 6);
     }
 }
 
-$row=1;
+$row = 0;
+$num_posts = $wp_query->post_count;
 while (have_posts()):
     echo '<div class="row">';
-    $fila=($row>3)?3:$row;
-    foreach ($aLayout[$fila] as $idcard=>$layout_fila){
-            $pos_color=((($row+1)%3)+$idcard)%3;
-            $card_bgcolor=$card_colors[$pos_color];
-            $layout=$layout_fila;
-            the_post(); 
-            //check if really there are a post
-            if (get_the_ID()){
-                reactor_post_before();
-                get_template_part('post-formats/format', "tac");
-                reactor_post_after();
-            } else{
+    $fila = ($row > 2) ? 2 : $row;
+    if ($num_posts < count($aLayout[$fila])) {
+        switch ($num_posts) {
+            case 1:
+                $columns = array(12);
                 break;
-            }
+            case 2:
+                $columns = array(6, 6);
+                break;
+            case 3:
+                $columns = array(4, 4, 4);
+                break;
+            case 4:
+                $columns = array(3, 3, 3, 3);
+                break;
+        }
+    } else {
+        $columns = $aLayout[$fila];
+    }
+    foreach ($columns as $idcard => $column_amplada) {
+        $card_bgcolor = $card_colors[((($row + 2) % 3) + $idcard + 1) % 3];
+        the_post();
+        $num_posts--;
+
+        // Check if really there are a post
+        if (!get_the_ID()) {
+            break;
+        }
+        reactor_post_before();
+        get_template_part('post-formats/format', "tac");
+        reactor_post_after();
     }
     $row++;
-    echo "</div>";  	
-endwhile;  	
+    echo "</div>";
+endwhile;
