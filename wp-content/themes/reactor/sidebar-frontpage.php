936f0a81c63f190872ee0cf04a9d28680e61911f
Sara Arjona <sarjona@xtec.cat>
2016-03-17 08:33:57 +0100

    Reactor: Fixed bug with menu icons and right column size

--- wp-content/themes/reactor/sidebar-frontpage.php
+++ wp-content/themes/reactor/sidebar-frontpage.php
@@ -9,7 +9,7 @@
 ?>
 <?php reactor_sidebar_before();
 $frontpage_layout = reactor_option('frontpage_layout');
-$pull = $frontpage_layout == '2c-r' || $frontpage_layout == '3c-c' ? -6 : -9;
+$pull = $frontpage_layout == '2c-r' || $frontpage_layout == '3c-c' ? -7 : -9;
 ?>
 <div id="sidebar-frontpage" class="sidebar <?php reactor_columns( 3 , true, false, null, $pull); ?>" role="complementary">
     <div id="sidebar-frontpage-inner">
