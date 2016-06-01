a9c967c120ccfa203737bcd2eba3493a63f81109
Sara Arjona <sarjona@xtec.cat>
2016-06-01 13:07:10 +0200

    Reviewed embed support in kses to let contributors add Picasa web albums

--- wp-includes/kses.php
+++ wp-includes/kses.php
@@ -171,13 +171,15 @@ if ( ! CUSTOM_TAGS ) {
 		'dl' => array(),
 		'dt' => array(),
 		'em' => array(),
-// XTEC ************ AFEGIT -
+// XTEC ************ AFEGIT - Let contributors use this tag
 		'embed' => array(
 			'height' => true,
 			'src' => true,
 			'style' => true,
 			'type' => true,
 			'width' => true,
+			'flashvars' => true,
+			'pluginspage' => true,
 		),
 //************ FI
 		'fieldset' => array(),
@@ -256,7 +258,7 @@ if ( ! CUSTOM_TAGS ) {
 			'style' => true,
 //************ FI
 		),
-// XTEC ************ AFEGIT -
+// XTEC ************ AFEGIT - Let contributors use this tag
 		'iframe' => array(
 			'align' => true,
 			'frameborder' => true,
@@ -315,7 +317,7 @@ if ( ! CUSTOM_TAGS ) {
 		),
 		's' => array(),
 		'samp' => array(),
-// XTEC ************ AFEGIT -
+// XTEC ************ AFEGIT - Let contributors use this tag
 		'script' => array(
 			'async' => true,
 			'charset' => true,
