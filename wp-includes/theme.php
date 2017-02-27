18d33b95a35ad943d59492367f385985fdf16e9e
xaviernietosanchez <xavier.nieto.sanchez@upcnet.es>
2017-02-27 14:20:26 +0100

    In themes list (wp-admin/themes.php), show only the theme already in use in that site

--- wp-includes/theme.php
+++ wp-includes/theme.php
@@ -67,25 +67,6 @@ function wp_get_themes( $args = array() ) {
 
 	foreach ( $theme_directories as $theme => $theme_root ) {
 
-// XTEC ************ AFEGIT - It only shows the correct theme in the themes selector
-// 2015.10.23 @dgras
-// 2017.01.30 @xaviernietosanchez
-		$CheckServei = isServeiEducatiu();
-		$CheckProject = isProjecte();
-
-        if( is_agora() && 
-            (( $CheckServei === true && strcmp( 'reactor-primaria-1', $theme ) === 0 ) ||
-             ( $CheckServei === true && strcmp( 'reactor-projectes', $theme ) === 0 ) ||
-             ( $CheckServei === false && strcmp( 'reactor-serveis-educatius', $theme ) === 0 ) ||
-             ( $CheckServei === false  && strcmp( 'reactor-projectes', $theme ) === 0 ) ||
-             ( $CheckProject === true && strcmp( 'reactor-primaria-1', $theme ) === 0 ) ||
-             ( $CheckProject === true && strcmp( 'reactor-serveis-educatius', $theme ) === 0 ) ||
-             ( $theme == 'reactor' ) )
-        ) {
-            continue;
-        }
-//************ FI
-
 		if ( isset( $_themes[ $theme_root['theme_root'] . '/' . $theme ] ) )
 			$themes[ $theme ] = $_themes[ $theme_root['theme_root'] . '/' . $theme ];
 		else
