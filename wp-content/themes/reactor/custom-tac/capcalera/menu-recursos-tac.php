bfddffc16ca4121f3882c2bef531c534be06812b
Toni Ginard <toni.ginard@gmail.com>
2019-11-11 10:42:47 +0100

    Accessibility improvements in themes

--- wp-content/themes/reactor/custom-tac/capcalera/menu-recursos-tac.php
+++ wp-content/themes/reactor/custom-tac/capcalera/menu-recursos-tac.php
@@ -1,123 +1,120 @@
 <?php
-function add_recursos( $wp_admin_bar ) {
+function add_recursos($wp_admin_bar) {
 
-	$args = array(
-		'id'     => 'recursosXTEC',
-		'title'  => '<img src="'.get_template_directory_uri().'/custom-tac/imatges/logo_xtec.png'."\">",
-		'parent' => false,
-	);
+    $args = [
+        'id' => 'recursosXTEC',
+        'title' => '<img alt ="Logotip XTEC" src="' . get_template_directory_uri() . '/custom-tac/imatges/logo_xtec.png' . "\">",
+        'parent' => false,
+    ];
 
-	$wp_admin_bar->add_node( $args );
+    $wp_admin_bar->add_node($args);
 
-	$args = array(
-		'id'     => 'xtec',
-		'title' => 'XTEC',
-		'href'=>'http://www.xtec.cat/',
-		'parent' => 'recursosXTEC',
-	);
+    $args = [
+        'id' => 'xtec',
+        'title' => 'XTEC',
+        'href' => 'http://www.xtec.cat/',
+        'parent' => 'recursosXTEC',
+    ];
 
-	$wp_admin_bar->add_node( $args );
+    $wp_admin_bar->add_node($args);
 
-	$args = array(
-		'id'     => 'edu365',
-		'href' => 'http://www.edu365.cat/',
-		'title' => 'Edu365',
-		'parent' => 'recursosXTEC',
-	);
+    $args = [
+        'id' => 'edu365',
+        'href' => 'http://www.edu365.cat/',
+        'title' => 'Edu365',
+        'parent' => 'recursosXTEC',
+    ];
 
-	$wp_admin_bar->add_node( $args );
+    $wp_admin_bar->add_node($args);
 
-	$args = array(
-		'id'     => 'edu3',
-		'href'=> 'http://www.edu3.cat/',
-		'title'  => 'Edu3',
-		'parent' => 'recursosXTEC',
-	);
+    $args = [
+        'id' => 'edu3',
+        'href' => 'http://www.edu3.cat/',
+        'title' => 'Edu3',
+        'parent' => 'recursosXTEC',
+    ];
 
-	$wp_admin_bar->add_node( $args );
+    $wp_admin_bar->add_node($args);
 
-	// XTEC ************ 601 - Add Sinapsi to the list of links XTEC (upper bar) #579 - 2018.07.13 @adriagarrido
-	$args = array(
-		'id' => 'sinapsi',
-		'href' => 'http://sinapsi.xtec.cat',
-		'title' => 'Sinapsi',
-		'parent' => 'recursosXTEC'
-	);
+    $args = [
+        'id' => 'sinapsi',
+        'href' => 'https://sinapsi.xtec.cat',
+        'title' => 'Sinapsi',
+        'parent' => 'recursosXTEC',
+    ];
 
-	$wp_admin_bar->add_node( $args );
+    $wp_admin_bar->add_node($args);
 
-	$args = array(
-		'id'     => 'alexandria',
-		'title'  => 'Alexandria',
-		'href' => 'http://alexandria.xtec.cat/',
-		'parent' => 'recursosXTEC',
-	);
+    $args = [
+        'id' => 'alexandria',
+        'title' => 'Alexandria',
+        'href' => 'http://alexandria.xtec.cat/',
+        'parent' => 'recursosXTEC',
+    ];
 
-	$wp_admin_bar->add_node( $args );
+    $wp_admin_bar->add_node($args);
 
-	$args = array(
-		'id'     => 'xarxadocent',
-		'title'  => 'Xarxa Docent',
-		'href'=>'http://educat.xtec.cat/',
-		'parent' => 'recursosXTEC',
-	);
+    $args = [
+        'id' => 'xarxadocent',
+        'title' => 'Xarxa Docent',
+        'href' => 'http://educat.xtec.cat/',
+        'parent' => 'recursosXTEC',
+    ];
 
-	$wp_admin_bar->add_node( $args );
+    $wp_admin_bar->add_node($args);
 
-	$args = array(
-		'id'     => 'arc',
-		'title'  => 'ARC',
-		'href' => 'http://apliense.xtec.cat/arc/',
-		'parent' => 'recursosXTEC',
-	);
+    $args = [
+        'id' => 'arc',
+        'title' => 'ARC',
+        'href' => 'http://apliense.xtec.cat/arc/',
+        'parent' => 'recursosXTEC',
+    ];
 
-	$wp_admin_bar->add_node( $args );
+    $wp_admin_bar->add_node($args);
 
-	$args = array(
-		'id'     => 'merli',
-		'title'  => 'Merlí',
-		'href'=>'http://aplitic.xtec.cat/merli/',
-		'parent' => 'recursosXTEC',
-	);
+    $args = [
+        'id' => 'merli',
+        'title' => 'Merlí',
+        'href' => 'http://aplitic.xtec.cat/merli/',
+        'parent' => 'recursosXTEC',
+    ];
 
-	$wp_admin_bar->add_node( $args );
+    $wp_admin_bar->add_node($args);
 
-	$args = array(
-		'id'     => 'jclic',
-		'title'  => 'jClic',
-		'href'	=> 'http://clic.xtec.cat/ca/index.htm',
-		'parent' => 'recursosXTEC',
-	);
+    $args = [
+        'id' => 'jclic',
+        'title' => 'jClic',
+        'href' => 'https://clic.xtec.cat/legacy/ca/index.html',
+        'parent' => 'recursosXTEC',
+    ];
 
-	$wp_admin_bar->add_node( $args );
+    $wp_admin_bar->add_node($args);
 
-	$args = array(
-		'id'     => 'linkat',
-		'title'  => 'Linkat',
-		'href' => 'http://linkat.xtec.cat/portal/index.php',
-		'parent' => 'recursosXTEC',
-	);
+    $args = [
+        'id' => 'linkat',
+        'title' => 'Linkat',
+        'href' => 'http://linkat.xtec.cat/portal/index.php',
+        'parent' => 'recursosXTEC',
+    ];
 
-	$wp_admin_bar->add_node( $args );
+    $wp_admin_bar->add_node($args);
 
-	$args = array(
-		'id'     => 'odissea',
-		'title'  => 'Odissea',
-		'href' => 'http://odissea.xtec.cat/',
-		'parent' => 'recursosXTEC',
-	);
+    $args = [
+        'id' => 'odissea',
+        'title' => 'Odissea',
+        'href' => 'https://odissea.xtec.cat/',
+        'parent' => 'recursosXTEC',
+    ];
 
-	$wp_admin_bar->add_node( $args );
+    $wp_admin_bar->add_node($args);
 
-	$args = array(
-		'id'     => 'agora',
-		'title'  => 'Àgora',
-		'href' => 'http://agora.xtec.cat/',
-		'parent' => 'recursosXTEC',
-	);
+    $args = [
+        'id' => 'agora',
+        'title' => 'Àgora',
+        'href' => 'https://agora.xtec.cat/',
+        'parent' => 'recursosXTEC',
+    ];
 
-	$wp_admin_bar->add_node( $args );
-
+    $wp_admin_bar->add_node($args);
 
 }
-?>
