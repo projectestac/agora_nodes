2dd6e6f8d3de28dd7e2f36ad2632df0c87d8eb83
Toni Ginard <toni.ginard@gmail.com>
2019-11-11 10:42:47 +0100

    Accessibility improvements in themes

--- wp-content/themes/reactor/custom-tac/capcalera/menu-logo.php
+++ wp-content/themes/reactor/custom-tac/capcalera/menu-logo.php
@@ -1,26 +1,25 @@
 <?php
 
-function add_logo( $wp_admin_bar ) {
-    
-    $isConsorci = stripos(reactor_option('cpCentre'),"barcelona")?true:false;
-    
+function add_logo($wp_admin_bar) {
+
+    $isConsorci = stripos(reactor_option('cpCentre'), "barcelona") ? true : false;
+
     if ($isConsorci) {
-	$args = array(
-		'id'     => 'consorci',    
-		'title'  => '<img src="'.get_template_directory_uri().'/custom-tac/imatges/logo_consorci.png">',
-		'href' =>'http://www.edubcn.cat/ca/',
-		'parent' => false,          
-	);
+        $args = [
+            'id' => 'consorci',
+            'title' => '<img alt="Logotip Consorci" src="' . get_template_directory_uri() . '/custom-tac/imatges/logo_consorci.png">',
+            'href' => 'https://www.edubcn.cat/ca/',
+            'parent' => false,
+        ];
     } else {
-        $args = array(
-		'id'     => 'gencat',    
-		'title'  => '<img src="'.get_template_directory_uri().'/custom-tac/imatges/logo_gene.png">',
-		'href' =>'http://www.gencat.cat/ensenyament/',
-		'parent' => false,
-        );
+        $args = [
+            'id' => 'gencat',
+            'title' => '<img alt="Logotip Generalitat" src="' . get_template_directory_uri() . '/custom-tac/imatges/logo_gene.png">',
+            'href' => 'https://www.gencat.cat/ensenyament/',
+            'parent' => false,
+        ];
     }
-        
-    $wp_admin_bar->add_menu( $args );
-        
+
+    $wp_admin_bar->add_menu($args);
+
 }
-?>
