7c919850f5a71d75514a3ab2fa9df46661f9217a
Toni Ginard <toni.ginard@gmail.com>
2022-04-21 16:50:19 +0200

    Changed defines by const when possible. Other minor tweaks.

--- site-config.php
+++ site-config.php
@@ -1,25 +1,25 @@
 <?php
 
-require_once (dirname(dirname(__FILE__)) . '/config/dblib-mysql.php');
+require_once (dirname(__FILE__, 2) . '/config/dblib-mysql.php');
 
 global $school_info;
 $centre = getSchoolInfo('nodes');
 
 global $agora, $isAgora, $isBlocs, $diskPercentNodes;
 
-if (isset($school_info['state_nodes']) && ($school_info['state_nodes'] == '-5')) {
+if (isset($school_info['state_nodes']) && ($school_info['state_nodes'] === '-5')) {
     setcookie($agora['server']['cookie'], '', time(), '/'); // Force cookie to expire
     header('Location: ' . WWWROOT . 'error.php?s=&migrating=' . $centre);
     exit();
 }
 
-if (isset($school_info['state_nodes']) && ($school_info['state_nodes'] == '-6')) {
+if (isset($school_info['state_nodes']) && ($school_info['state_nodes'] === '-6')) {
     setcookie($agora['server']['cookie'], '', time(), '/'); // Force cookie to expire
     header('Location: ' . WWWROOT . 'error.php?s=&migrated=' . $centre);
     exit();
 }
 
-if (isset($school_info['state_nodes']) && ($school_info['state_nodes'] == '-7')) {
+if (isset($school_info['state_nodes']) && ($school_info['state_nodes'] === '-7')) {
     setcookie($agora['server']['cookie'], '', time(), '/'); // Force cookie to expire
     header('Location: ' . WWWROOT . 'error.php?s=&saturated=' . $centre);
     exit();
@@ -36,13 +36,15 @@ define('ENVIRONMENT', $agora['server']['enviroment']);
 define('SCHOOL_CODE', $school_info['clientCode']);
 
 // Check for subdomain
-if (!empty($school_info['url_type']) && ($school_info['url_type'] == 'subdomain') && !empty($school_info['url_host'])) {
+if (!empty($school_info['url_type']) && ($school_info['url_type'] === 'subdomain') && !empty($school_info['url_host'])) {
     define('WP_SITEURL', $agora['server']['html']);
+    define('WP_HOME', $agora['server']['html']);
 } else {
     define('WP_SITEURL', $agora['server']['html'] . $centre . '/');
+    define('WP_HOME', $agora['server']['html'] . $centre . '/');
 }
 
-$diskPercentNodes = (isset($school_info['diskPercent_nodes'])) ? $school_info['diskPercent_nodes'] : 0;
+$diskPercentNodes = $school_info['diskPercent_nodes'] ?? 0;
 
 if (isset($agora['iseoi']) && $agora['iseoi']) {
     define('XTEC_MAIL_IDAPP', 'AGORAEOI');
