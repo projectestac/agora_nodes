20ac72385b8e548c44828242460584a15477ef1d
Toni Ginard <toni.ginard@gmail.com>
2022-04-21 16:50:19 +0200

    Changed defines by const when possible. Other minor tweaks.

--- wp-config.php
+++ wp-config.php
@@ -25,8 +25,8 @@ include_once 'site-config.php';
 // ** MySQL settings ** //
 define('DB_USER', $agora['nodes']['username']);
 define('DB_PASSWORD', $agora['nodes']['userpwd']);
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
+const DB_CHARSET = 'utf8';
+const DB_COLLATE = '';
 
 // Proxy configuration
 if (!empty($agora['proxy']['host'])) {
@@ -39,22 +39,22 @@ if (!empty($agora['proxy']['user'])) {
 }
 
 // Force https on login
-define('FORCE_SSL_ADMIN', true);
+const FORCE_SSL_ADMIN = true;
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
     $_SERVER['HTTPS'] = 'on';
 }
 
 // Force usage of MySQLi instead of MySQL
-define ('WP_USE_EXT_MYSQL', false);
+const WP_USE_EXT_MYSQL = false;
 
 // Block any kind of filesystem change, automatic or by users
-define('DISALLOW_FILE_MODS', true);
+const DISALLOW_FILE_MODS = true;
 
 // Completely disable all types of automatic updates, core or otherwise
-define('AUTOMATIC_UPDATER_DISABLED', true);
+const AUTOMATIC_UPDATER_DISABLED = true;
 
 // Disable asynchronous default cron
-define('DISABLE_WP_CRON', 'true');
+const DISABLE_WP_CRON = true;
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -65,14 +65,14 @@ define('DISABLE_WP_CRON', 'true');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'q$ATvJpV<JO`Xa)7hv!4)u%<wSdmtE?519V>8X#ds~|Pp9|jduz1[Cc:1??K%ty:');
-define('SECURE_AUTH_KEY',  ']|R`MQ?~b$|e`)R^},O1FFT|H_x!ukyhEXr:bOO2Fq;a_..iRNG;b,tzYNFx|i&v');
-define('LOGGED_IN_KEY',    'o[E|Hg}oL89sDHB9,cPm(+bd!klzS}R=e7o]N=6N$@^s]u?z@{ SA032!@c-2lzj');
-define('NONCE_KEY',        'Y.ojKh*XW<&*bZ?0]b/g/_?klFzk}O&J-}$-#>}x7Ye<E[U`,4ih@& 5MqUo}L~:');
-define('AUTH_SALT',        '(Ym[t///b5JXmf9W(g..Fv2L@qF5473IQ|F4C/|},Y[F{SRhlL?ADKWa/@8ts&-s');
-define('SECURE_AUTH_SALT', '#m=Q-MjIv4v}B7Ewp3IG@~N!DwE=eUoyK><[V^CoZ6A[ltJ0jrvD??L-,^J;DA~6');
-define('LOGGED_IN_SALT',   'Rr& 6>;jA1?K?xW}zWsvxE@i6lS4*+f|RH0W[}.:egl|RYQ*Y2FVLqvx^~}GiO8%');
-define('NONCE_SALT',       'N+=]R(*9^T2<ja;$x61F}tq6XDNO%v[iL.?3#;h9Y@lJ2-X%72>- 6!3B8R}g:uN');
+const AUTH_KEY = 'q$ATvJpV<JO`Xa)7hv!4)u%<wSdmtE?519V>8X#ds~|Pp9|jduz1[Cc:1??K%ty:';
+const SECURE_AUTH_KEY = ']|R`MQ?~b$|e`)R^},O1FFT|H_x!ukyhEXr:bOO2Fq;a_..iRNG;b,tzYNFx|i&v';
+const LOGGED_IN_KEY = 'o[E|Hg}oL89sDHB9,cPm(+bd!klzS}R=e7o]N=6N$@^s]u?z@{ SA032!@c-2lzj';
+const NONCE_KEY = 'Y.ojKh*XW<&*bZ?0]b/g/_?klFzk}O&J-}$-#>}x7Ye<E[U`,4ih@& 5MqUo}L~:';
+const AUTH_SALT = '(Ym[t///b5JXmf9W(g..Fv2L@qF5473IQ|F4C/|},Y[F{SRhlL?ADKWa/@8ts&-s';
+const SECURE_AUTH_SALT = '#m=Q-MjIv4v}B7Ewp3IG@~N!DwE=eUoyK><[V^CoZ6A[ltJ0jrvD??L-,^J;DA~6';
+const LOGGED_IN_SALT = 'Rr& 6>;jA1?K?xW}zWsvxE@i6lS4*+f|RH0W[}.:egl|RYQ*Y2FVLqvx^~}GiO8%';
+const NONCE_SALT = 'N+=]R(*9^T2<ja;$x61F}tq6XDNO%v[iL.?3#;h9Y@lJ2-X%72>- 6!3B8R}g:uN';
 /**#@-*/
 
 /**
@@ -90,27 +90,35 @@ $table_prefix  = $agora['nodes']['prefix'] . '_';
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', false);
+const WP_DEBUG = false;
+const WP_DEBUG_DISPLAY = false;
+const WP_DEBUG_LOG = false;
 
 // Deactivate the loading of compressed Javascript and CSS files
-define('SCRIPT_DEBUG', false);
+const SCRIPT_DEBUG = false;
 
 // Activates the saving of database queries in an array ($wpdb->queries).
 // Set to true and uncomment code in theme reactor-primaria-1 in footer.php
 //  and in wp-admin/admin-footer.php for admin pages, to use it.
-define('SAVEQUERIES', false);
+const SAVEQUERIES = false;
 
 // Default blog creation theme.
-define('WP_DEFAULT_THEME', 'NODES');
+const WP_DEFAULT_THEME = 'NODES';
 
 // Autosave time
-define('AUTOSAVE_INTERVAL', 300); // 5 minuts
-// Number of revisions of each post
-define('WP_POST_REVISIONS', 3);
+const AUTOSAVE_INTERVAL = 300; // 5 minutes
 
-// Reduce the number of requests
+// Number of revisions of each post
+const WP_POST_REVISIONS = 3;
+
+// Reduce the number of requests in admin pages
 const CONCATENATE_SCRIPTS = true;
 
+// Performance tweaks
+const COMPRESS_SCRIPTS = true;
+const COMPRESS_CSS = true;
+
+const WP_LOCAL_DEV = false;
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
