b23ac9bd8a9ecd7fb0e3ee5b80dabdd9ac150d8a
Toni Ginard <toni.ginard@gmail.com>
2023-05-19 11:31:41 +0200

    alumni-fp: Updated plugin

--- wp-content/plugins/alumni-fp/alumni-fp.php
+++ wp-content/plugins/alumni-fp/alumni-fp.php
@@ -26,21 +26,19 @@ function extensions_dashboard_widget_alumni_fp() {
 
     if (current_user_can('activate_plugins')) {
         echo '
-            <div id="dashboard_box_alumni_fp">
             <div style="text-align:center">
-                <img height="150px" src="https://projectes.xtec.cat/impulsfp/wp-content/uploads/usu2078/2023/03/AlumniFP.png">
+                <img height="150px" src="https://projectes.xtec.cat/impulsfp/wp-content/uploads/usu2078/2023/03/AlumniFP.png" alt="" />
             </div>
             <p>
-                <strong>Alumni FP</strong> és un projecte per ajudar als centres d\'FP a crear comunitats d’Alumni amb la finalitat de mantenir els lligams entre el centre educatiu i l’exalumnat.
+                <strong>Alumni FP</strong> és un projecte per ajudar als centres d\'FP a crear comunitats d\'Alumni amb la finalitat de mantenir els lligams entre el centre educatiu i l\'exalumnat.
             </p>
             <div style="text-align:center">
                 <a class="button" href="https://projectes.xtec.cat/impulsfp/alumnifp/">Més informació</a>
             </div>
-            <p>Per importar un espai Alumini FP en el Nodes del vostre centre segueix aquests passos:</p>
-            1. <a target="_blank" href="https://ja.cat/alumnifp-export">Descarrega</a> el fitxer que conté les pàgines de l\'espai Alumni. És un fitxer amb extensió xml.<br /><br />
-            2. <a target="_blank" href="admin.php?import=wordpress">Importa</a> el fitxer descarregat. Es crearan un conjunt de pàgines relacionades amb Alumni FP.<br /><br />
-            3. <a target="_blank" href="' . get_permalink($page->ID) . '">Visita</a> l\'espai AlumniFP en el teu Nodes.
-            </div>
+            <p>Per importar un espai Alumini FP en el Nodes del vostre centre seguiu aquests passos:</p>
+            1. <a target="_blank" href="https://ja.cat/alumnifp-export">Descarregueu</a> el fitxer que conté les pàgines de l\'espai Alumni. És un fitxer amb extensió xml.<br /><br />
+            2. <a target="_blank" href="admin.php?import=wordpress">Importeu</a> el fitxer descarregat marcant "Descarregar i importar adjunts". Es crearan un conjunt de pàgines relacionades amb Alumni FP.<br /><br />
+            3. Afegiu la <a target="_blank" href="' . home_url() . '/lexalumnat-4/">pàgina inicial d\'Alumni</a> al menú del vostre web.
         ';
     }
 }
