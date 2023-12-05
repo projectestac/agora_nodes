e70db8e45d4e7e20b7523e1ef2b67bad70d85a1c
Toni Ginard <toni.ginard@gmail.com>
2023-12-05 11:49:47 +0100

    Theme reactor: Removed some custom code for Internet Explorer

--- wp-content/themes/reactor/header.php
+++ wp-content/themes/reactor/header.php
@@ -6,10 +6,7 @@
  * @subpackge Templates
  * @since 1.0.0
  */?><!DOCTYPE html>
-<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
-<!--[if ( IE 7 )&!( IEMobile )]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
-<!--[if ( IE 8 )&!( IEMobile )]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+<html <?php language_attributes(); ?> class="no-js">
 
 <head>
     <!-- Add common styles to all themes -->
