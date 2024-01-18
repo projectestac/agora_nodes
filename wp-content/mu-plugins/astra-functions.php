83b104c6e0353fc5c3663c85f676f6d09de397e0
Toni Ginard <toni.ginard@gmail.com>
2024-01-18 16:19:14 +0100

    WIP: Customizations for Nodes 2

---
+++ wp-content/mu-plugins/astra-functions.php
@@ -0,0 +1,221 @@
+<?php
+/*
+Plugin Name: AstraFunctions
+Plugin URI: https://github.com/projectestac/agora_nodes
+Description: Customizations for Astra theme
+Version: 1.0
+Author: Departament d'Educació - Generalitat de Catalunya
+*/
+
+// Only load if Astra theme is active.
+if (wp_get_theme()->name !== 'Astra') {
+    return;
+}
+
+include_once WPMU_PLUGIN_DIR . '/astra-compatibility/get-options.php';
+include_once WPMU_PLUGIN_DIR . '/astra-compatibility/customizer/customize.php';
+include_once WPMU_PLUGIN_DIR . '/astra-widgets/logo-client-widget.php';
+
+// Load styles to customize Astra theme.
+function astra_load_css(): void {
+    wp_register_style('astra-functions-css', plugins_url('/astra-styles/style.css', __FILE__));
+    wp_enqueue_style('astra-functions-css');
+}
+add_action('wp_enqueue_scripts', 'astra_load_css');
+
+// Admin bar: Force to be always shown, including for non-logged users.
+add_filter('show_admin_bar', '__return_true');
+
+// Admin bar: Remove WordPress logo.
+add_action('wp_before_admin_bar_render', 'nodes_admin_bar_remove_logo');
+function nodes_admin_bar_remove_logo(): void {
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_node('wp-logo');
+}
+
+// Admin bar: Add Departament d'Educació logo in the first position and menu with XTEC resources.
+add_action('admin_bar_menu', 'nodes_modify_admin_bar');
+function nodes_modify_admin_bar($wp_admin_bar): void {
+
+    $wp_admin_bar->add_node([
+        'id' => 'dept-educacio-logo-wrapper',
+        'parent' => '',
+        'title' => '<img id="logo-dept-educacio" alt="Logo Educació" src="' . WPMU_PLUGIN_URL . '/astra-images/logo_gene.png' . '">',
+        'href' => 'https://educacio.gencat.cat/ca/inici/',
+        'meta' => [
+            'tabindex' => -1,
+        ],
+    ]);
+
+    $wp_admin_bar->add_node([
+        'id' => 'recursosXTEC',
+        'title' => '<img id="logo-xtec" alt="Logotip XTEC" src="' . WPMU_PLUGIN_URL . '/astra-images/logo_xtec.png' . '">',
+        'parent' => false,
+    ]);
+
+    $wp_admin_bar->add_node([
+        'id' => 'xtec',
+        'title' => 'XTEC',
+        'href' => 'https://xtec.gencat.cat/ca/inici',
+        'parent' => 'recursosXTEC',
+    ]);
+
+    $wp_admin_bar->add_node([
+        'id' => 'edu365',
+        'href' => 'https://www.edu365.cat/',
+        'title' => 'Edu365',
+        'parent' => 'recursosXTEC',
+    ]);
+
+    $wp_admin_bar->add_node([
+        'id' => 'edu3',
+        'href' => 'http://www.edu3.cat/',
+        'title' => 'Edu3',
+        'parent' => 'recursosXTEC',
+    ]);
+
+    $wp_admin_bar->add_node([
+        'id' => 'sinapsi',
+        'href' => 'https://sinapsi.xtec.cat',
+        'title' => 'Sinapsi',
+        'parent' => 'recursosXTEC',
+    ]);
+
+    $wp_admin_bar->add_node([
+        'id' => 'alexandria',
+        'title' => 'Alexandria',
+        'href' => 'https://alexandria.xtec.cat/',
+        'parent' => 'recursosXTEC',
+    ]);
+
+    $wp_admin_bar->add_node([
+        'id' => 'arc',
+        'title' => 'ARC',
+        'href' => 'https://apliense.xtec.cat/arc/',
+        'parent' => 'recursosXTEC',
+    ]);
+
+    $wp_admin_bar->add_node([
+        'id' => 'merli',
+        'title' => 'Merlí',
+        'href' => 'http://aplitic.xtec.cat/merli/',
+        'parent' => 'recursosXTEC',
+    ]);
+
+    $wp_admin_bar->add_node([
+        'id' => 'jclic',
+        'title' => 'jClic',
+        'href' => 'https://clic.xtec.cat/legacy/ca/index.html',
+        'parent' => 'recursosXTEC',
+    ]);
+
+    $wp_admin_bar->add_node([
+        'id' => 'linkat',
+        'title' => 'Linkat',
+        'href' => 'http://linkat.xtec.cat/portal/index.php',
+        'parent' => 'recursosXTEC',
+    ]);
+
+    $wp_admin_bar->add_node([
+        'id' => 'odissea',
+        'title' => 'Odissea',
+        'href' => 'https://odissea.xtec.cat/',
+        'parent' => 'recursosXTEC',
+    ]);
+
+    $wp_admin_bar->add_node([
+        'id' => 'agora',
+        'title' => 'Àgora',
+        'href' => 'https://educaciodigital.cat/',
+        'parent' => 'recursosXTEC',
+    ]);
+
+    if (!is_user_logged_in()) {
+        $wp_admin_bar->add_node([
+            'parent' => 'top-secondary',
+            'id' => 'login-link-admin-bar',
+            'title' => __('Log in'),
+            'href' => wp_login_url(),
+        ]);
+    }
+
+}
+
+// Customizer: Remove all Astra sections.
+add_filter('astra_header_builder_sections', 'nodes_remove_customizer_header_footer_sections');
+add_filter('astra_footer_builder_sections', 'nodes_remove_customizer_header_footer_sections');
+add_filter('astra_customizer_sections', 'nodes_remove_customizer_general_sections');
+
+function nodes_remove_customizer_general_sections ($configurations): array {
+    if (is_xtec_super_admin()) {
+        return $configurations;
+    }
+
+    return array_filter($configurations, static function ($configuration) {
+        return $configuration['type'] !== 'section';
+    });
+}
+
+function nodes_remove_customizer_header_footer_sections($configurations): array {
+    if (is_xtec_super_admin()) {
+        return $configurations;
+    }
+
+    return [];
+}
+
+// Customizer: Add custom sections.
+add_action('customize_register', 'nodes_add_customizer_sections');
+function nodes_add_customizer_sections($wp_customize): void {
+    // Secció Color de Fons
+    $wp_customize->add_section('nodes_header', [
+        'title' => __('Header', 'astra-nodes'),
+        'priority' => 30,
+    ]);
+
+    // Configuració de Color de Fons
+    $wp_customize->add_setting('color_fons_setting', [
+        'default' => '#ffffff',
+        'transport' => 'refresh',
+    ]);
+
+    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'color_fons_control', [
+        'label' => __('Selecciona el color de fons', 'astra-nodes'),
+        'section' => 'nodes_header',
+        'settings' => 'color_fons_setting',
+    ]));
+}
+
+
+
+
+
+
+
+
+
+//add_filter('astra_get_option_header-html-3', 'custom_header_html_3', 120, 0);
+function custom_header_html_3() {
+//    $content = astra_get_option('header-html-1');
+    return '<div class="tipus-centre">Centre formatiu</div>
+<h1>Institut Agustí Serra i Fontanet</h1>
+<h2><span style="color: #00b856; font-size: 18pt;">ESO Batxillerat Cicles Formatius</span></h2>';
+}
+
+//add_action('wp_footer', 'meu_tema_elimina_personalitzador', 200);
+function meu_tema_elimina_personalitzador() {
+    global $wp_customize;
+    // Treure la secció Color de Fons
+    //$wp_customize->remove_section('panel-global');
+    $wp_customize->remove_panel('panel-global');
+
+    // O bé, pots treure controls específics
+    //$wp_customize->remove_control('astra-global');
+}
+
+//add_action('customize_register', 'llistar_seccions_personalitzador', 120);
+function llistar_seccions_personalitzador($wp_customize) {
+    foreach ($wp_customize->panels() as $key => $object ) {
+        echo $key . ' - ' . $object->title . "\n";
+    }
+}
---
+++ wp-content/mu-plugins/astra-widgets/logo-client-widget.php
@@ -0,0 +1,213 @@
+<?php
+
+class Logo_Client_Widget extends WP_Widget {
+
+    // Create widget
+    public function __construct() {
+        parent::__construct(
+            'logo_client_widget', // ID base
+            __('Client card', 'astra-nodes'), // Name
+            ['description' => 'Mostra les dades i, opcionalment, el logotip del centre. La informació es defineix a Aparença -> Personalitza -> Identificació del centre']
+        );
+    }
+
+    // Front-End Display of the Widget
+    public function widget($args, $instance): void {
+
+        // Saved widget options
+        $title = $instance['title'] ?? '';
+        echo $args['before_widget'];
+
+        if (!empty($title)) {
+            echo '<h4 class="widget-title">' . $title . '</h4>';
+        }
+
+        // Check options to print url link or mailto link
+        $contacteCentre = reactor_option('contacteCentre');
+        $correuCentre = reactor_option('correuCentre');
+        $contacte_mobile_enabled = false;
+
+        (!empty($contacteCentre)) ? $contacte = $contacteCentre : $contacte = false;
+        (!empty($correuCentre)) ? $contacte_mobile = "mailto:" . $correuCentre : $contacte_mobile = false;
+
+        if (!$contacte && $contacte_mobile) {
+            $contacte = $contacte_mobile;
+            $contacte_mobile_enabled = true;
+        } else if (!$contacte_mobile && $contacte) {
+            $contacte_mobile = $contacte;
+        }
+
+        ?>
+        <div class="targeta_id_centre row">
+            <?php
+            if (reactor_option('logo_image')) {
+                if (reactor_option('logo_inline')) {
+                    $class_logo = 'logo_inline';
+                    $class_addr = 'addr-centre';
+                    $amplada = '6';
+                } else {
+                    $class_logo = 'logo_clear';
+                    $class_addr = 'logo_clear';
+                    $amplada = '12';
+                }
+                ?>
+                <div class="<?php echo $class_logo; ?> hide-for-small">
+                    <img alt="Logotip entitat" id="logo_entity" src="<?php
+                    echo reactor_option('logo_image'); ?>"/>
+                </div>
+                <?php
+            }
+            ?>
+
+            <?php
+            [$postal_code, $locality] = explode(' ', reactor_option('cpCentre'), 2);
+            ?>
+            <div class="<?php echo $class_addr; ?> ">
+                <div class="vcard">
+                    <span id="tar-nomCentre"><?php
+                        echo reactor_option('nomCanonicCentre'); ?></span>
+                    <div class="adr">
+                        <span class="street-address"><?php
+                            echo reactor_option('direccioCentre'); ?></span><br>
+                        <span class="postal-code"><?php
+                            echo trim($postal_code); ?></span>
+                        <span class="locality"><?php
+                            echo trim($locality); ?></span>
+                        <span class="region" title="Catalunya">Catalunya</span>
+                        <span class="country-name">Espanya</span>
+                        <div class="tel">
+                            <span><?php
+                                echo reactor_option('telCentre'); ?></span>
+                        </div>
+                        <?php
+
+                        // Get home url to check behavior target link
+                        $currentDomain = get_home_url();
+                        $searchDomain = ['http://', 'https://'];
+                        $currentDomain = str_replace($searchDomain, '', $currentDomain);
+                        $contacteDomain = str_replace($searchDomain, '', $contacte);
+
+                        // Check if googleMaps is empty to show or not
+                        $showPipe = false;
+                        $emptyMaps = reactor_option('googleMaps');
+                        if (!empty($emptyMaps)) {
+                            $showPipe = true;
+                            if (strpos(reactor_option('googleMaps'), $currentDomain) !== false) {
+                                ?>
+                                <a id="tar-mapa" href="<?php
+                                echo reactor_option('googleMaps'); ?>">mapa</a>
+                                <?php
+                            } else if (strpos(reactor_option('googleMaps'), 'http') === false) {
+                                if (strpos(reactor_option('googleMaps'), '.') !== false) {
+                                    ?>
+                                    <a id="tar-mapa" target="_blank" href="<?php
+                                    echo esc_url("http://" . reactor_option('googleMaps')); ?>">mapa</a>
+                                    <?php
+                                } else if (strpos(trim(reactor_option('googleMaps')), '/') === 0) {
+                                    ?>
+                                    <a id="tar-mapa" href=" <?php
+                                    echo esc_url(get_home_url() . reactor_option('googleMaps')); ?>">mapa</a>
+                                    <?php
+                                } else {
+                                    ?>
+                                    <a id="tar-mapa" href=" <?php
+                                    echo esc_url(get_home_url() . '/' . reactor_option('googleMaps')); ?>">mapa</a>
+                                    <?php
+                                }
+                            } else {
+                                ?>
+                                <a id="tar-mapa" target="_blank" href="<?php
+                                echo reactor_option('googleMaps'); ?>">mapa</a>
+                                <?php
+                            }
+                        }
+                        ?>
+                        <?php
+                        // Check if $contacte is empty to show or not
+                        if ($contacte && $contacte_mobile) {
+                            if ($showPipe) {
+                                ?>
+                                <span class="pipe">|</span>
+                                <?php
+                            }
+
+                            if ($contacte_mobile_enabled) {
+                                ?>
+                                <a id="tar-contacte" href="<?php
+                                echo esc_url($contacte); ?>">contacte</a>
+                                <?php
+                            } else if (strpos($contacteDomain, $currentDomain) !== false) {
+                                ?>
+                                <a id="tar-contacte" href="<?php
+                                echo $contacte; ?>">contacte</a>
+                                <?php
+                            } else {
+                                if (strpos($contacte, 'http') === false) {
+                                    if (strpos($contacte, '.') !== false) {
+                                        ?>
+                                        <a id="tar-contacte" target="_blank" href="<?php
+                                        echo esc_url("https://" . $contacte); ?>">contacte</a>
+                                        <?php
+                                    } else if (strpos(trim($contacte), '/') === 0) {
+                                        ?>
+                                        <a id="tar-contacte" href="<?php
+                                        echo esc_url($currentDomain . $contacte); ?>">contacte</a>
+                                        <?php
+                                    } else {
+                                        ?>
+                                        <a id="tar-contacte" href="<?php
+                                        echo esc_url($currentDomain . '/' . $contacte); ?>">contacte</a>
+                                        <?php
+                                    }
+                                } else {
+                                    ?>
+                                    <a id="tar-contacte" target="_blank" href="<?php
+                                    echo esc_url($contacte); ?>">contacte</a>
+                                    <?php
+                                }
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+        echo $args['after_widget']; ?>
+        <?php
+    }
+
+    // End function widget
+    // Back-end form of the Widget
+    public function form($instance): void {
+
+        // Check for values
+        if (isset($instance['title'])) {
+            $title = $instance['title'];
+        }
+        ?>
+        <p>
+            <label for="<?php
+            echo $this->get_field_id('title'); ?>">Títol:</label>
+            <input class="widefat" id="<?php
+            echo $this->get_field_id('title'); ?>" name="<?php
+            echo $this->get_field_name('title'); ?>" type="text" value="<?php
+            echo esc_attr($title); ?>">
+        </p>
+
+        <?php
+    }
+
+    // Sanitize and return the safe form values
+    public function update($new_instance, $old_instance): array {
+        $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        return $instance;
+    }
+
+}
+
+// Register widget
+add_action('widgets_init', function () {
+    register_widget('logo_client_widget');
+}
+);
