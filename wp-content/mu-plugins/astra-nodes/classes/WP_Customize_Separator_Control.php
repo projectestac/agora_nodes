16ac665d33d3640a036a0a9564d7b05ac3e3c427
Toni Ginard <toni.ginard@gmail.com>
2024-04-08 20:25:42 +0200

    Theme Astra Nodes: Added front page slider to customizer (step 1)

---
+++ wp-content/mu-plugins/astra-nodes/classes/WP_Customize_Separator_Control.php
@@ -0,0 +1,14 @@
+<?php
+
+class WP_Customize_Separator_Control extends WP_Customize_Control {
+    public $type = 'separator';
+
+    public function __construct($manager, $id, $args = []) {
+        parent::__construct($manager, $id, $args);
+    }
+
+    public function render_content(): void {
+        echo '<br><hr><br>';
+    }
+
+}
