d85e1c1cdf665622f4fa2d06d60a038ff7b018d0
Toni Ginard <toni.ginard@gmail.com>
2024-04-08 13:47:40 +0200

    Theme Astra Nodes: Improvements to color selector

--- wp-content/mu-plugins/astra-nodes/classes/WP_Customize_Palette_Control.php
+++ wp-content/mu-plugins/astra-nodes/classes/WP_Customize_Palette_Control.php
@@ -10,23 +10,25 @@ class WP_Customize_Palette_Control extends WP_Customize_Control {
 
         $astra_color_palettes = get_option('astra-color-palettes');
         $palettes = $astra_color_palettes['palettes'];
+        $currentPalette = $astra_color_palettes['currentPalette'];
 
         echo '<div class="palette-control">';
         echo '<div class="ast-color-palette-container">';
 
         foreach ($palettes as $name => $palette) {
-            echo '';
-            echo '
-                 <div class="" style="width:125px; float: left; border: 1px solid #cccccc;">
-                    <div class="" style="width:23%; height:48px; display:inline-block; background-color:' . $palette[0] . ';"></div>
-                    <div class="" style="width:23%; height:48px; display:inline-block; background-color:' . $palette[1] . ';"></div>
-                    <div class="" style="width:23%; height:48px; display:inline-block; background-color:' . $palette[2] . ';"></div>
-                    <div class="" style="width:23%; height:48px; display:inline-block; background-color:' . $palette[3] . ';"></div>
-                    <span class="ast-palette-label-wrap">'
-                    . $name . '<input type="radio" name="palette" value="' . $name . '" style="" data-customize-setting-link="astra-color-palettes[currentPalette]">
-                    </span>
-                </div>
-        ';
+            $border = ($name === $currentPalette) ? '2px solid #000000;' : '1px solid #cccccc;';
+            echo '<label for="' . $name . '">';
+            echo '<div class="astra-nodes-palette-container" style="border:' . $border . '">';
+            echo '<div class="astra-nodes-palette-item" style="background-color:' . $palette[0] . ';"></div>';
+            echo '<div class="astra-nodes-palette-item" style="background-color:' . $palette[1] . ';"></div>';
+            echo '<div class="astra-nodes-palette-item" style="background-color:' . $palette[2] . ';"></div>';
+            echo '<div class="astra-nodes-palette-item" style="background-color:' . $palette[3] . ';"></div>';
+            echo '<span class="astra-nodes-palette-footer">'
+                . $name
+                . '<input type="radio" id="' . $name . '" name="palette" value="' . $name . '"data-customize-setting-link="astra-color-palettes[currentPalette]">';
+            echo '</span>';
+            echo '</div>';
+            echo '</label>';
         }
 
         echo '</div>';
@@ -35,7 +37,17 @@ class WP_Customize_Palette_Control extends WP_Customize_Control {
     }
 
     public function enqueue(): void {
-//        wp_enqueue_style('astra-css', content_url('wp-content/mu-plugins/astra-nodes/styles/style.css'));
+        wp_enqueue_style('astra-nodes-css', content_url('mu-plugins/astra-nodes/styles/style.css'));
+        wp_register_script('astra-nodes-js-palette', '', ['jquery'], '', true);
+        wp_enqueue_script('astra-nodes-js-palette');
+        wp_add_inline_script('astra-nodes-js-palette', "
+            jQuery(document).ready(function() {
+                jQuery('.astra-nodes-palette-item').on('click', function() {
+                    jQuery('.astra-nodes-palette-container').css('border', '1px solid #cccccc');
+                    jQuery(this).parent().css('border', '2px solid #000000');
+                });
+            });
+        ");
     }
 
 }
