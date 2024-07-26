1f16b08b06c300f461220a943e2f38cf76e84411
Toni Ginard <toni.ginard@gmail.com>
2024-07-26 10:03:08 +0200

    astra-nodes: Customizer: Improved layout selector

--- wp-content/mu-plugins/astra-nodes/classes/WP_Customize_Layout_Control.php
+++ wp-content/mu-plugins/astra-nodes/classes/WP_Customize_Layout_Control.php
@@ -7,32 +7,46 @@ class WP_Customize_Layout_Control extends WP_Customize_Control {
     }
 
     public function render_content(): void {
+
         $current_value = $this->value();
 
         echo '<div class="layout-control">';
 
         $i = 0;
         foreach ($this->choices as $value => $label) {
+
             $select_class = ($value === $current_value) ? 'box-selected' : 'box-unselected';
             echo '<label for="' . $value . '">';
             echo '<div class="astra-nodes-layout-container ' . $select_class . '">';
-            echo '<span class="astra-nodes-layout-item">'
-                . $label
+
+            // Title.
+            echo '<div class="astra-nodes-layout-item">'
+                . '<div class="layout-title">' . $label . '</div>'
                 . '<input type="radio" id="' . $value . '" name="layout" value="' . $value . '"';
             $this->link();
             checked($current_value, $value);
             echo '>';
+
+            // Separator.
             echo '<hr>';
-            echo '<img src="' . WPMU_PLUGIN_URL . '/astra-nodes/images/layout_' . $i . '.png" alt="layout_' . $i . '.png">';
-            echo '</span>';
+
+            // Image.
+            echo '<img class="astra-nodes-customizer-layout-images"
+                       src="' . WPMU_PLUGIN_URL . '/astra-nodes/images/layout_' . $i . '.png" alt="layout_' . $i . '.png">';
+            echo '</div>';
             echo '</div>';
             echo '</label>';
+
             $i++;
+
         }
+
         echo '</div>';
+
     }
 
     public function enqueue(): void {
+
         wp_enqueue_style('astra-nodes-layout-css', content_url('mu-plugins/astra-nodes/styles/style.css'));
         wp_register_script('astra-nodes-layout-js', '', ['jquery'], '', true);
         wp_enqueue_script('astra-nodes-layout-js');
@@ -45,4 +59,5 @@ class WP_Customize_Layout_Control extends WP_Customize_Control {
             });
         ");
     }
+
 }
