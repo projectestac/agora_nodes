6b8b6e32a03c91b5be3d2c6c922a5fe35294254e
Toni Ginard <toni.ginard@gmail.com>
2026-06-03 10:57:55 +0200

    Nodes 2: Customizer: Front page notice: Added support for links in visual editor

--- wp-content/mu-plugins/astra-nodes/classes/WP_Customize_TinyMCE_Control.php
+++ wp-content/mu-plugins/astra-nodes/classes/WP_Customize_TinyMCE_Control.php
@@ -1,36 +1,49 @@
 <?php
 
-class WP_Customize_TinyMCE_Control extends WP_Customize_Control {
+class WP_Customize_TinyMCE_Control extends WP_Customize_Control
+{
 
-    public function __construct($manager, $id, $args = []) {
+    public function __construct($manager, $id, $args = [])
+    {
         parent::__construct($manager, $id, $args);
     }
 
-    public function render_content(): void {
+    public function render_content(): void
+    {
+        // Generate a clean ID, without brackets, so that TinyMCE doesn't break.
+        $safe_id = 'tinymce_' . md5($this->id);
         ?>
         <label>
             <span class="customize-control-title">
-                <?php echo esc_html($this->label); ?>
+                <?= esc_html($this->label) ?>
             </span>
 
-            <div class="customize-control-content">
-                <textarea id="customize-control-<?= esc_attr($this->id) ?>"
+            <span class="customize-control-content">
+                <textarea id="<?= esc_attr($safe_id) ?>"
                           class="customize-control-tinymce"
-                          <?php $this->link(); ?>>
-                    <?= esc_textarea($this->value()) ?>
-                </textarea>
-            </div>
+                          <?php $this->link(); ?>
+                          ><?= esc_textarea($this->value()) ?></textarea>
+            </span>
         </label>
         <?php
     }
 
-    public function enqueue(): void {
+    public function enqueue(): void
+    {
+        wp_enqueue_editor();
+
         wp_enqueue_script(
             'customizer-tinymce',
             get_site_url() . '/wp-content/mu-plugins/astra-nodes/customizer/js/customizer-tinymce.js',
             ['jquery', 'customize-controls'],
             false,
             true
+        );
+
+        // These styles force the TinyMCE modal window to go past the Customizer.
+        wp_add_inline_style(
+                'customize-controls',
+                '.mce-window, .mce-window-backdrop, .tox-dialog-wrap { z-index: 500000 !important; }'
         );
     }
 }
